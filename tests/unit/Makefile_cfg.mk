dd085c10755e625376650b26e45c22efa4b294b8
yyang42 <yaoyanyang42@gmail.com>
2015-12-06 00:11:02 +0100

    makefile: update test makefile

--- tests/unit/Makefile_cfg.mk
+++ tests/unit/Makefile_cfg.mk
@@ -2,5 +2,5 @@ NAME = test.out
 RENDU_PATH = ../..
 FRAMEWORK_PATH = ../../../moulitest/testframework/v3
 CC_LIBS = -ltowel -L $(RENDU_PATH)/libs/libtowel -I $(RENDU_PATH)/libs/libtowel/includes
-RENDU_SOURCE = $(RENDU_PATH)/srcs/**/*.c
+RENDU_SOURCE = $(shell find $(RENDU_PATH)/srcs -name "*.c" -type f -follow -print)
 RENDU_MAKE_ARG =
