efbd58c423fb9c897aa4225f683495971efc72f1
Marc Giraud-telme <mgiraud-@student.42.fr>
2015-12-08 18:29:33 +0100

    remove useless code for options

--- srcs/xopt/xopt_init.c
+++ srcs/xopt/xopt_init.c
@@ -12,20 +12,8 @@
 
 #include "xopt.h"
 
-void				print_usage(void)
-{
-	twl_printf("usage: ./ft_nm <file>\n");
-	twl_printf("\t-n     Sort numerically rather than alphabetically.\n");
-}
-
 void				xopt_init(t_xopt *xopt, char **av)
 {
 	xopt->opt__ = twl_opt_new(av, XOPT_VALID_OPTS);
 	xopt_check_valid_opts(xopt);
-	if (twl_opt_exist(xopt->opt__, "h"))
-	{
-		print_usage();
-		exit(0);
-	}
-	// xopt->help = twl_opt_exist(xopt->opt__, "h");
 }
