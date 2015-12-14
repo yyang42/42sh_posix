485bbe9d3f27ecb9f36ffe82d8dfc28a68db26cf
Ludovic CHENUT <lchenut@e2r11p17.42.fr>
2015-12-14 18:24:06 +0100

    #productivity

--- srcs/patmatch/pattern/pattern_is_directory.c
+++ srcs/patmatch/pattern/pattern_is_directory.c
@@ -6,31 +6,17 @@
 /*   By: chuck <chuck@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2042/02/30 42:00:00 by chuck             #+#    #+#             */
-/*   Updated: 2042/02/30 42:59:59 by chuck            ###   ########.fr       */
+/*   Updated: 2042/02/30 41:59:59 by chuck            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "patmatch.h"
+#include "pattern.h"
 
-static char			**pat_to_string(t_pattern *pattern)
+int					pattern_is_directory(t_pattern *this, int depth)
 {
-	char			**ret;
-
-	if (!(ret = (char **)malloc(sizeof(char *) * 2)))
-		return (NULL);
-	ret[1] = NULL;
-	ret[0] = pattern_to_string(pattern);
-	return (ret);
-}
-
-char				**patmatch_match(t_patmatch *this, char *pattern_)
-{
-	t_pattern		*pattern;
-	t_lst			*lst_pat;
-
-	pattern = pattern_new(pattern_);
-	lst_pat = twl_lst_new();
-	twl_lst_del(lst_pat, &free);
-	pattern_del(pattern);
-	return (NULL);
+	if (depth < 0 || depth + 1 >= pattern_get_depth(this))
+	{
+		return (0);
+	}
+	return (1);
 }
