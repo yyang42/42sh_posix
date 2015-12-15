13f7a0ce8f426aff5777873547c8a9c1e852d262
yyang42 <yaoyanyang42@gmail.com>
2015-12-15 21:33:57 +0100

    leaks: fix tools/valgrind.sh exit status

--- tools/valgrind.sh
+++ tools/valgrind.sh
@@ -12,16 +12,23 @@ valgrind --leak-check=full --log-file="$LOGFILE" $BASEDIR/../tests/unit/test.out
 cat $LOGFILE
 
 cat $LOGFILE
+
+error=0
+
 if cat $LOGFILE | grep -q "definitely lost: 0 bytes in 0 blocks"
 then
 	echo $C_GREEN"=== Leaks test success   ==="$C_CLEAR
 else
 	echo   $C_RED"=== Leaks test fail      ==="$C_CLEAR
+	error=1
 fi
 
 if cat $LOGFILE | grep -q "Invalid read"
 then
 	echo   $C_RED"=== Invalid read fail    ==="$C_CLEAR
+	error=1
 else
 	echo $C_GREEN"=== Invalid read success ==="$C_CLEAR
 fi
+
+exit $error
