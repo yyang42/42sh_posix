835a8b02485f03a1500a1f8fa026b356d802972d
yyang42 <yaoyanyang42@gmail.com>
2015-12-17 20:14:41 +0100

    ast: add leaks tests

--- tests/ast_diff_tests/ast_diff_tests.sh
+++ tests/ast_diff_tests/ast_diff_tests.sh
@@ -7,7 +7,7 @@ C_CLEAR="\033[0m"
 
 RENDU_PATH="`pwd`"
 TESTS_ROOT_PATH="$RENDU_PATH/tests/ast_diff_tests"
-
+LOGFILE=$RENDU_PATH/.valgrind.log
 exit_status=0
 
 make
@@ -39,15 +39,34 @@ diff_test ()
     mkdir -p $testcase_tmp
     rm -f $testcase_tmp/*
     $RENDU_PATH/42sh -z $testcase_path/input.sh > $testcase_tmp_stdout 2> $testcase_tmp_stderr
-    diff $testcase_path/expected_stdout $testcase_tmp_stdout
-    print_result "$?"
-    echo " stdout $testsuite/$testcase"
-    diff $testcase_path/expected_stderr $testcase_tmp_stderr
-    print_result "$?"
-    echo " stderr $1/$2"
+
+    if `env | grep -q ^LEAK_TEST=`
+    then
+        valgrind --leak-check=full --log-file="$LOGFILE" $RENDU_PATH/42sh -z $testcase_path/input.sh
+        if grep -q "definitely lost: 0 bytes in 0 blocks" $LOGFILE || grep -q "in use at exit: 0 bytes in 0 blocks" $LOGFILE
+        then
+            echo $C_GREEN"No Leaks $testcase_path"$C_CLEAR
+        else
+            cat $LOGFILE
+            echo   $C_RED"=== Leaks test fail      ==="$C_CLEAR
+            exit 1
+        fi
+        if grep -q "Invalid read"; then
+            cat $LOGFILE
+            echo   $C_RED"=== Leaks test fail      ==="$C_CLEAR
+            exit 1
+        fi
+    else
+        diff $testcase_path/expected_stdout $testcase_tmp_stdout
+        print_result "$?"
+        echo " stdout $testsuite/$testcase"
+        diff $testcase_path/expected_stderr $testcase_tmp_stderr
+        print_result "$?"
+        echo " stderr $1/$2"
+    fi
 }
 
-echo $C_CYAN"====== START AST DIFF TESTS ======"$C_CLEAR
+echo $C_CYAN"====== START AST TESTS ======"$C_CLEAR
 if ! `env | grep -q ^LAST_ONLY=`
 then
 
