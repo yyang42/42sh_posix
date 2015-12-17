eb5356d46ddbdcb1ad6e7ab10e4e25c97d8765d4
Antoine TAMANO <atamano@e2r7p15.42.fr>
2015-12-17 15:55:11 +0100

    feat: [simple cmd] add binary path checking

---
+++ srcs/error/error.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_norris_loves_the_norminette.c                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chuck <chuck@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2042/02/30 42:00:00 by chuck             #+#    #+#             */
+/*   Updated: 2042/02/30 41:59:59 by chuck            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "error.h"
+
+void custom_error(const char *format, ...)
+{
+	va_list args;
+
+	twl_dprintf (2, format, args);
+	twl_dprintf(2, "\n");
+}
--- srcs/simple_command/get_binary_path.c
+++ srcs/simple_command/get_binary_path.c
@@ -10,6 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "error.h"
 #include "simple_command.h"
 <<<<<<< 5cfe5110949bd6ea648812288938a171cf733ed5:srcs/simple_command/get_binary_path.c
 #include "environment.h"
@@ -17,15 +18,50 @@
 #include "builtin.h"
 >>>>>>> Links with builtin:srcs/simple_command/simple_command_new.c
 
-char	*get_binary_path(char *cmd)
+static	int check_file_existence(char *path, bool show_error)
+{
+	if (!file_exists(path))
+	{
+		if (show_error)
+			custom_error("42sh: no such file or directory: %s", path);
+		return (0);
+	}
+	else if (!file_isexecutable(path))
+	{
+		custom_error("42sh: %s: Permission denied", path);
+		return (-1);
+	}
+	return (1);
+}
+
+char		*get_binary_path(char *cmd)
 {
 <<<<<<< 5cfe5110949bd6ea648812288938a171cf733ed5:srcs/simple_command/get_binary_path.c
 	t_environment	*env;
 	char			**paths;
+	char			*path;
+	int 			i;
+	int				flag;
 
 	env = environment_singleton();
 	paths = environment_get_paths(env);
-	(void)cmd;
+	if (cmd && (cmd[0] == '/' || twl_strncmp(cmd, "./", 2) == 0))
+	{
+		if (check_file_existence(path, true) <= 0)
+			return (NULL);
+	}
+	i = -1;
+	while (paths[++i])
+	{
+		path = twl_joinpath(paths[i], cmd);
+		flag = check_file_existence(path, false)
+		if (flag < 0)
+			return (NULL);
+		else if (flag == 1)
+			return (path);
+		free(path);
+	}
+	custom_error("42sh: no such file or directory: %s", path);
 	return (NULL);
 =======
 	(void)dict;
