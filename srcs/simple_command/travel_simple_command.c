1577b4fc117eec27ef590a81aa595577a77bda65
Marc Giraud-telme <mgiraud-@student.42.fr>
2015-12-18 15:47:26 +0100

    remove debug

--- srcs/simple_command/travel_simple_command.c
+++ srcs/simple_command/travel_simple_command.c
@@ -20,12 +20,10 @@ void travel_simple_command(void *ast_node,  void *cmd_)
 	if (ast_node_get_type(ast_node) == AST_LIST)
 	{
 		t_ast_list		*ast_list = ast_node;
-		twl_printf("%s\n", "Root Detected");
 		twl_lst_iter(ast_list->items, travel_simple_command, cmd);
 	}
 	else if (ast_node_get_type(ast_node) == AST_CMD)
 	{
-		twl_printf("%s\n", "AST CMD Detected");
 		travel_ast_cmd(cmd, ast_node);
 	}
 }
