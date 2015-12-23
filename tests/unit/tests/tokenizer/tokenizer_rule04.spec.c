e1ef97ff8647eecf334bf8622da00aa0c2d16009
yyang42 <yaoyanyang42@gmail.com>
2015-12-23 21:13:47 +0100

    ast_parser: handle rule 4 partially add test case

--- tests/unit/tests/tokenizer/tokenizer_rule04.spec.c
+++ tests/unit/tests/tokenizer/tokenizer_rule04.spec.c
@@ -10,6 +10,7 @@ mt_test_tokenizer(num4, "a \"1$(a b c) 2\" b", 	    "a_\"1$(a b c) 2\"_b", false
 mt_test_tokenizer(num5, "\\ ", 	                    "\\ ", false);
 mt_test_tokenizer(num6, "a '1 \\\"2' b", 	        "a_'1 \\\"2'_b", false);
 mt_test_tokenizer(num7, "a&'1 2'&3", 	            "a_&_'1 2'_&_3", false);
+mt_test_tokenizer(num8, "a b'c d'e f", 	            "a_b'c d'e_f", false);
 
 void	suite_tokenizer_rule04(t_suite *suite)
 {
@@ -20,4 +21,5 @@ void	suite_tokenizer_rule04(t_suite *suite)
 	SUITE_ADD_TEST(suite, test_num5);
 	SUITE_ADD_TEST(suite, test_num6);
 	SUITE_ADD_TEST(suite, test_num7);
+	SUITE_ADD_TEST(suite, test_num8);
 }
