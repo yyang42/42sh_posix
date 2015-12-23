7ed27e268540ddefab808ac8859489ca9f33bc08
ju <julien.schaeffer@gmail.com>
2015-12-23 09:11:08 +0100

    !!!

--- tests/unit/Makefile_cfg.mk
+++ tests/unit/Makefile_cfg.mk
@@ -3,4 +3,4 @@ RENDU_PATH = ../..
 FRAMEWORK_PATH = ../../../moulitest/testframework/v3
 CC_LIBS = -lncurses -ltowel -L $(RENDU_PATH)/libs/libtowel -I $(RENDU_PATH)/libs/libtowel/includes -I $(RENDU_PATH)/includes/builtin
 RENDU_SOURCE = $(shell find $(RENDU_PATH)/srcs -name "*.c" -type f -follow -print)
-RENDU_MAKE_ARG =
+RENDU_MAKE_ARG = -lncurses
