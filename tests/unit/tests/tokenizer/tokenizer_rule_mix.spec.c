230df228f8995316c30800b8e642533db502bd49
yyang42 <yaoyanyang42@gmail.com>
2015-12-25 14:35:58 +0100

    tokenizer: refactor tests

--- tests/unit/tests/tokenizer/tokenizer_rule_mix.spec.c
+++ tests/unit/tests/tokenizer/tokenizer_rule_mix.spec.c
@@ -3,15 +3,15 @@
 #include "tokenizer.h"
 #include "token_mgr.h"
 
-mt_test_tokenizer(num1, "> wc",  ">_wc", false);
-mt_test_tokenizer(num2, ">\nwc",  ">_\n_wc", false);
-mt_test_tokenizer(num3, ">  wc",  ">_wc", false);
-mt_test_tokenizer(num04, "ls  &&  wc||date",  "ls_&&_wc_||_date", false);
+mt_test_tokenizer(num01, "> wc",                 ">_wc",                 false);
+mt_test_tokenizer(num02, ">\nwc",                ">_\n_wc",              false);
+mt_test_tokenizer(num03, ">  wc",                ">_wc",                 false);
+mt_test_tokenizer(num04, "ls  &&  wc||date",    "ls_&&_wc_||_date",     false);
 
-void	suite_tokenizer_rule_mix(t_suite *suite)
+void    suite_tokenizer_rule_mix(t_suite *suite)
 {
-	SUITE_ADD_TEST(suite, test_num1);
-	SUITE_ADD_TEST(suite, test_num2);
-	SUITE_ADD_TEST(suite, test_num3);
-	SUITE_ADD_TEST(suite, test_num04);
+    SUITE_ADD_TEST(suite, test_num01);
+    SUITE_ADD_TEST(suite, test_num02);
+    SUITE_ADD_TEST(suite, test_num03);
+    SUITE_ADD_TEST(suite, test_num04);
 }
