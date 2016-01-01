c2de645a459fd6c755e5e5bf4e3df6e1a3ec2676
yyang42 <yaoyanyang42@gmail.com>
2016-01-01 21:22:43 +0100

    ast_parser: add no new line ast test case

--- tests/ast_diff_tests/ast_diff_tests.sh
+++ tests/ast_diff_tests/ast_diff_tests.sh
@@ -55,6 +55,7 @@ then
     diff_test features_ast 01_empty
     diff_test features_ast 10_simple_command
     diff_test features_ast 11_simple_command_empty_lines
+    diff_test features_ast 12_simple_command_no_newline
     diff_test features_ast 20_subshell
     diff_test features_ast 30_redir
     diff_test features_ast 40_assignment
