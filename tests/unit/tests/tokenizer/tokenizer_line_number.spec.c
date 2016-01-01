d45d94e4e033055a8c5dc53e6cb7436d55731f39
yyang42 <yaoyanyang42@gmail.com>
2016-01-01 16:52:14 +0100

    tokenizer: ci debug true

--- tests/unit/tests/tokenizer/tokenizer_line_number.spec.c
+++ tests/unit/tests/tokenizer/tokenizer_line_number.spec.c
@@ -20,11 +20,12 @@
 		mt_assert(strcmp(actual, expected) == 0); \
 	}
 
-mt_test_tokenizer_line_no(num1, "echo abc 123\n", "echo(1:1)_abc(1:6)_123(1:10)_\n(1:13)", false);
-mt_test_tokenizer_line_no(num2, "echo abc\necho bbb\n", "echo(1:1)_abc(1:6)_\n(1:9)_echo(2:1)_bbb(2:6)_\n(2:9)", false);
-mt_test_tokenizer_line_no(num3, "echo 'a\nb' 123\n", "echo(1:1)_'a\nb'(1:6)_123(2:4)_\n(2:7)", false);
-mt_test_tokenizer_line_no(num4, "echo 'a\n\nb' 123\n", "echo(1:1)_'a\n\nb'(1:6)_123(3:4)_\n(3:7)", false);
-mt_test_tokenizer_line_no(num5, "\n", "\n(1:1)", false);
+mt_test_tokenizer_line_no(num1, "echo abc 123\n", "echo(1:1)_abc(1:6)_123(1:10)_\n(1:13)", true);
+mt_test_tokenizer_line_no(num2, "echo abc\necho bbb\n", "echo(1:1)_abc(1:6)_\n(1:9)_echo(2:1)_bbb(2:6)_\n(2:9)", true);
+mt_test_tokenizer_line_no(num3, "echo 'a\nb' 123\n", "echo(1:1)_'a\nb'(1:6)_123(2:4)_\n(2:7)", true);
+mt_test_tokenizer_line_no(num4, "echo 'a\n\nb' 123\n", "echo(1:1)_'a\n\nb'(1:6)_123(3:4)_\n(3:7)", true);
+mt_test_tokenizer_line_no(num5, "\n", "\n(1:1)", true);
+mt_test_tokenizer_line_no(num6, "", "", true);
 
 void	suite_tokenizer_line_number(t_suite *suite)
 {
@@ -33,4 +34,5 @@ void	suite_tokenizer_line_number(t_suite *suite)
 	SUITE_ADD_TEST(suite, test_num3);
 	SUITE_ADD_TEST(suite, test_num4);
 	SUITE_ADD_TEST(suite, test_num5);
+	SUITE_ADD_TEST(suite, test_num6);
 }
