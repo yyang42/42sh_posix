ed1c3442c15a894f4f79535a3225cbfcf29eb965
yyang42 <yaoyanyang42@gmail.com>
2016-01-01 16:56:41 +0100

    tokenizer: ci debug false

--- tests/unit/tests/tokenizer/tokenizer_line_number.spec.c
+++ tests/unit/tests/tokenizer/tokenizer_line_number.spec.c
@@ -16,16 +16,16 @@
 			printf("actual    {%s}\n", actual); \
 			printf("expected  {%s}\n", expected); \
 		} \
-		free(actual); \
 		mt_assert(strcmp(actual, expected) == 0); \
+		free(actual); \
 	}
 
-mt_test_tokenizer_line_no(num1, "echo abc 123\n", "echo(1:1)_abc(1:6)_123(1:10)_\n(1:13)", true);
-mt_test_tokenizer_line_no(num2, "echo abc\necho bbb\n", "echo(1:1)_abc(1:6)_\n(1:9)_echo(2:1)_bbb(2:6)_\n(2:9)", true);
-mt_test_tokenizer_line_no(num3, "echo 'a\nb' 123\n", "echo(1:1)_'a\nb'(1:6)_123(2:4)_\n(2:7)", true);
-mt_test_tokenizer_line_no(num4, "echo 'a\n\nb' 123\n", "echo(1:1)_'a\n\nb'(1:6)_123(3:4)_\n(3:7)", true);
-mt_test_tokenizer_line_no(num5, "\n", "\n(1:1)", true);
-mt_test_tokenizer_line_no(num6, "", "", true);
+mt_test_tokenizer_line_no(num1, "echo abc 123\n", "echo(1:1)_abc(1:6)_123(1:10)_\n(1:13)", false);
+mt_test_tokenizer_line_no(num2, "echo abc\necho bbb\n", "echo(1:1)_abc(1:6)_\n(1:9)_echo(2:1)_bbb(2:6)_\n(2:9)", false);
+mt_test_tokenizer_line_no(num3, "echo 'a\nb' 123\n", "echo(1:1)_'a\nb'(1:6)_123(2:4)_\n(2:7)", false);
+mt_test_tokenizer_line_no(num4, "echo 'a\n\nb' 123\n", "echo(1:1)_'a\n\nb'(1:6)_123(3:4)_\n(3:7)", false);
+mt_test_tokenizer_line_no(num5, "\n", "\n(1:1)", false);
+mt_test_tokenizer_line_no(num6, "", "", false);
 
 void	suite_tokenizer_line_number(t_suite *suite)
 {
