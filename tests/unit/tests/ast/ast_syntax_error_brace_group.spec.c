d56922bbdfdb2e2ba96fd1858fe2c3a8a32df660
yyang42 <yaoyanyang42@gmail.com>
2016-01-03 12:11:01 +0100

    ast_parser: add test for brace_group

---
+++ tests/unit/tests/ast/ast_syntax_error_brace_group.spec.c
@@ -0,0 +1,13 @@
+#include <project.h>
+
+#include "ast/ast.h"
+
+mt_test_ast_error(01, "{cmd_level_1}; {\n", "SyntaxError 1:16 : Closing token for '{' not found", false);
+
+mt_test_ast_error(02, "{ { }\n", "SyntaxError 1:1 : Closing token for '{' not found", false);
+
+void	suite_ast_syntax_error_brace_group(t_suite *suite)
+{
+	SUITE_ADD_TEST(suite, test_01);
+	SUITE_ADD_TEST(suite, test_02);
+}
