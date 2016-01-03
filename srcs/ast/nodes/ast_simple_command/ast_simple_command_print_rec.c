997b0d2643e568ed85009d77adf27efa10a9512a
yyang42 <yaoyanyang42@gmail.com>
2016-01-03 10:18:31 +0100

    ast_parser: improve ast output

--- srcs/ast/nodes/ast_simple_command/ast_simple_command_print_rec.c
+++ srcs/ast/nodes/ast_simple_command/ast_simple_command_print_rec.c
@@ -16,12 +16,14 @@
 
 void				ast_simple_command_print_rec(t_ast_simple_command *this, int depth)
 {
-	//char			*joined_tokens;
+	char			*joined_tokens;
 
-	// joined_tokens = token_mgr_strjoin(this->command_tokens, "_");
+	joined_tokens = token_mgr_strjoin(this->command_tokens, " ");
 	ast_print_indent(depth);
-	twl_printf("ast_simple_command (%s)\n", token_mgr_first(this->command_tokens)->text);
+	twl_printf("ast_simple_command (%s)\n", joined_tokens);
+	free(joined_tokens);
 	depth++;
 	ast_redir_print_rec_list(this->redir_items, depth);
 	ast_assignment_print_rec_list(this->assignment_items, depth);
+
 }
