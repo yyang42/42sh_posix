4c5399c064dfc89451ffb1d7320c2af25bc9ec05
Antoine TAMANO <atamano@e2r9p12.42.fr>
2016-01-06 16:13:25 +0100

    feat: [ast redir] add duplication fd checking

---
+++ srcs/ast/nodes/ast_simple_command/ast_simple_command_check_duplicate.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_norris_loves_the_norminette.c                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chuck <chuck@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2042/02/30 42:00:00 by chuck             #+#    #+#             */
+/*   Updated: 2042/02/30 41:59:59 by chuck            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ast/nodes/ast_redir.h"
+#include "ast/nodes/ast_assignment.h"
+#include "ast/nodes/ast_simple_command.h"
+
+int			get_duplication_fd(char *str)
+{
+	int fd;
+
+	if (twl_str_is_pos_num(str))
+	{
+		fd = twl_atoi(str);
+		if (fd > getdtablesize())
+		{
+			twl_dprintf(2, "42sh: %d: Bad file descriptor", fd);
+			return (-1);
+		}
+		return (fd);
+	}
+	else
+	{
+		twl_dprintf(2, "42sh: %s: ambiguous redirect", str);
+		return (-1);
+	}
+}
