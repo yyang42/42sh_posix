20f81f047442c07705aa90423e0810053a9622bb
Marc GIRAUD-TELME <mgiraud-@e2r8p11.42.fr>
2016-01-19 14:34:58 +0100

    EXPAN WIP better parser for parameter substitution

---
+++ srcs/ast/expan/expan_quote/expan_quote_new.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_norris_loves_the_norminette.c                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chuck <chuck@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2042/02/30 42:00:00 by chuck             #+#    #+#             */
+/*   Updated: 2042/02/30 41:59:59 by chuck            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ast/expan/ast_expan_quote.h"
+
+t_expan_quote	*expan_quote_new(void)
+{
+	t_expan_quote *quote;
+
+	quote = twl_malloc_x0(sizeof(t_expan_quote));
+	quote->str = NULL;
+	quote->is_single_quoted = false;
+	quote->is_double_quoted = false;
+	quote->is_backslashed = false;
+	return (quote);
+}
--- srcs/ast/expan/expan_tokenizer/expan_tokenizer_param_substitution.c
+++ srcs/ast/expan/expan_tokenizer/expan_tokenizer_param_substitution.c
@@ -13,6 +13,7 @@
 #include "ast/expan/ast_expan_tokenizer.h"
 #include "ast/expan/ast_expan_param.h"
 #include "ast/expan/ast_expan_utils.h"
+#include "ast/expan/ast_expan_quote.h"
 
 
 t_expan_param_type	expan_tokenizer_param_substitution_get_operator(char *str, int *j)
@@ -33,38 +34,175 @@ t_expan_param_type	expan_tokenizer_param_substitution_get_operator(char *str, in
 	return (string_1_to_expan_parameter_type(operator));
 }
 
+// static int	expan_tokenizer_param_substitution_get_parameter_word(t_expan_param *expan_param,
+// 	char *str, int i)
+// {
+// 	int					j;
+// 	t_expan_param_type	type;
+// 	int					word_start;
+// 	int					op_len;
+//
+// 	j = i;
+// 	word_start = i;
+// 	op_len = 0;
+// 	while (str[j] != 0 && (str[j] != '}'))
+// 	{
+// 		type = expan_tokenizer_param_substitution_get_operator(&str[j], &op_len);
+// 		if (type != UNDEFINED_PARAM)
+// 		{
+// 			expan_param->parameter = twl_strndup(&str[i], j - i);
+// 			expan_param->type = type;
+// 			j += op_len;
+// 			word_start = j;
+// 		}
+// 		else
+// 			j++;
+// 	}
+// 	if (word_start == i)
+// 		expan_param->parameter = twl_strndup(&str[i], j - i);
+// 	else
+// 		expan_param->word = twl_strndup(&str[word_start], j - word_start);
+// 	if (str[j] == '}')
+// 		j++;
+// 	return (j);
+// }
+
+static bool expan_tokenizer_param_substitution_get_parameter(t_expan_param *expan_param, char *str, char *sep_addr)
+{
+	if (sep_addr == str)
+	{
+		twl_dprintf(2, "42sh: bad substitution\n");
+		return (false);
+	}
+	expan_param->parameter = twl_strndup(str, sep_addr - str);
+	return (true);
+}
+
+void iter_fn_setup_quotes(void *quote_, void *prev_quote_, void *context_)
+{
+	t_expan_quote	*quote;
+	t_expan_quote	*prev_quote;
+
+	(void)context_;
+	quote = quote_;
+	prev_quote = prev_quote_;
+	if (!twl_strcmp(quote->str,"'") || !twl_strcmp(quote->str,"\"") || !twl_strcmp(quote->str,"\\"))
+	{
+		if (prev_quote)
+		{
+			if (!twl_strcmp(quote->str,"'") && twl_strcmp(prev_quote->str, "\\"))
+				quote->is_single_quoted = !quote->is_single_quoted;
+			else if (!twl_strcmp(quote->str,"\"") && twl_strcmp(prev_quote->str, "\\"))
+				quote->is_double_quoted = !quote->is_double_quoted;
+			else if (!twl_strcmp(quote->str,"\"") && twl_strcmp(prev_quote->str, "\\"))
+				quote->is_backslashed = true;
+		}
+		else
+		{
+			if (!twl_strcmp(quote->str,"'"))
+				quote->is_single_quoted = !quote->is_single_quoted;
+			else if (!twl_strcmp(quote->str,"\""))
+				quote->is_double_quoted = !quote->is_double_quoted;
+		}
+	}
+	else if (prev_quote)
+	{
+		if (!twl_strcmp(prev_quote->str,"'") && prev_quote->is_backslashed == false)
+			quote->is_single_quoted = prev_quote->is_single_quoted;
+		else if (!twl_strcmp(prev_quote->str, "'") && prev_quote->is_backslashed == true)
+			quote->is_single_quoted = !prev_quote->is_single_quoted;
+		if (!twl_strcmp(prev_quote->str, "\"") && prev_quote->is_backslashed == false)
+			quote->is_double_quoted = prev_quote->is_double_quoted;
+		else if (!twl_strcmp(prev_quote->str, "\"") && prev_quote->is_backslashed == true)
+			quote->is_double_quoted = !prev_quote->is_double_quoted;
+		if (!twl_strcmp(prev_quote->str, "\\"))
+			quote->is_backslashed = true;
+	}
+}
+
+void		iter_fn_get_word(void *quote_, void *len_, void *nb_open_brace_, void *finished_)
+{
+	t_expan_quote	*quote;
+	int				*len;
+	int				*nb_open_brace;
+	bool			*finished;
+
+	quote = quote_;
+	len = len_;
+	nb_open_brace = nb_open_brace_;
+	finished = finished_;
+	if (*finished == false)
+	{
+		if (!twl_strcmp(quote->str, "{") && !quote->is_double_quoted && !quote->is_single_quoted && !quote->is_backslashed)
+		{
+			*nb_open_brace = *nb_open_brace + 1;
+			*len = *len + 1;
+		}
+		else if (!twl_strcmp(quote->str, "}") && !quote->is_double_quoted && !quote->is_single_quoted && !quote->is_backslashed)
+		{
+			*nb_open_brace = *nb_open_brace - 1;
+			if (*nb_open_brace == 0)
+				*finished = true;
+			else
+				*len = *len + 1;
+		}
+		else
+			*len = *len + 1;
+	}
+}
+
+static bool expan_tokenizer_param_substitution_get_word(t_expan_param *expan_param, char *str)
+{
+	int				word_len;
+	t_lst			*quotes;
+	t_expan_quote	*quote;
+	int				nb_open_brace;
+	bool			finished;
+
+	word_len = 0;
+	quotes = twl_lst_new();
+	nb_open_brace = 1;
+	finished = false;
+	while (str[word_len] != 0)
+	{
+		quote = expan_quote_new();
+		quote->str = twl_strndup(&str[word_len], 1);
+		word_len++;
+	}
+	word_len = 0;
+	twl_lst_iterp(quotes, iter_fn_setup_quotes, NULL);
+	twl_lst_iter3(quotes, iter_fn_get_word, &word_len, &nb_open_brace, &finished);
+	twl_lst_del(quotes, expan_quote_del);
+	(void)expan_param;
+	return (true);
+	// expan_param->parameter = twl_strndup(str, sep_addr - str);
+}
+
 static int	expan_tokenizer_param_substitution_get_parameter_word(t_expan_param *expan_param,
 	char *str, int i)
 {
 	int					j;
-	t_expan_param_type	type;
-	int					word_start;
+	char				*sep_addr;
 	int					op_len;
 
 	j = i;
-	word_start = i;
+	sep_addr = &str[j];
 	op_len = 0;
-	while (str[j] != 0 && str[j] != '}')
+	while (str[j] != 0)
 	{
-		type = expan_tokenizer_param_substitution_get_operator(&str[j], &op_len);
-		if (type != UNDEFINED_PARAM)
+		if (expan_tokenizer_param_substitution_get_operator(&str[j], &op_len) != UNDEFINED_PARAM)
 		{
-			expan_param->parameter = twl_strndup(&str[i], j - i);
-			expan_param->type = type;
-			j += op_len;
-			word_start = j;
+			sep_addr = &str[j];
+			break;
 		}
 		else
 			j++;
 	}
-	if (word_start == i)
-		expan_param->parameter = twl_strndup(&str[i], j - i);
-	else
-		expan_param->word = twl_strndup(&str[word_start], j - word_start);
-	if (str[j] == '}')
-		j++;
+	if (expan_tokenizer_param_substitution_get_parameter(expan_param, &str[i], sep_addr))
+		expan_tokenizer_param_substitution_get_word(expan_param, sep_addr + op_len);
 	return (j);
 }
+
 
 int		expan_tokenizer_param_substitution(t_expan_token *expan_token,
 	char *str, int i)
