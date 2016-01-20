37e21c0651ddad810264fbadb4299bb95c584a8a
Ludovic <lchenut@student.42.fr>
2016-01-20 15:18:51 +0100

    pattern matching: added bracket_expr_get

---
+++ srcs/pattern_matching/bracket_expression/bracket_expr_get.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_norris_loves_the_norminette.c                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chuck <chuck@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2042/02/30 42:00:00 by chuck             #+#    #+#             */
+/*   Updated: 2042/02/30 41:59:59 by chuck            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pattern_matching/bracket_expr.h"
+
+char			*bracket_expr_get(t_bracket_expr *this, char *key)
+{
+	return (twl_dict_get(this->dict, key));
+}
