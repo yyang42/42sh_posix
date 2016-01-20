571c0fab921a7397c001fe648b148b01488f311f
Ludovic <lchenut@student.42.fr>
2016-01-20 15:12:16 +0100

    pattern matching: added bracket_expr_add

---
+++ srcs/pattern_matching/bracket_expression/bracket_expr_add.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_norris_loves_the_norminette.c                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chuck <chuck@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2042/02/30 42:00:00 by chuck             #+#    #+#             */
+/*   Updated: 2042/02/30 41:59:59 by chuck            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pattern_matching/bracket_expr.h"
+
+void			bracket_expr_add(t_bracket_expr *this, char *key, char *val)
+{
+	twl_dict_add(this->dict, key, val);
+}
