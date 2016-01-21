390c9b6845bf5042aec0286bc43d4588df5b80a5
Ludovic <lchenut@student.42.fr>
2016-01-21 13:29:13 +0100

    pattern matching: improve tests again

--- tests/unit/tests/patmatch/pattern/pattern_to_string.spec.c
+++ tests/unit/tests/patmatch/pattern/pattern_to_string.spec.c
@@ -1,44 +1,55 @@
 #include <project.h>
 #include "pattern_matching/pattern.h"
 
-#define PATTERN_TEST(a, b) \
-{                                                       \
-    t_pattern   *tmp;                                   \
-    char        *actual;                                \
-    tmp = pattern_new(a);                               \
-    actual = pattern_to_string(tmp);                    \
-    mt_assert(strcmp(actual, b) == 0);                  \
-    pattern_del(tmp);                                   \
-    free(actual);                                       \
+#define mt_test_pattern_tostring(test_name, input, expected, debug)	\
+static void test_## test_name(t_test *test)							\
+{																	\
+	t_pattern	*tmp = pattern_new(input);							\
+	char		*actual = pattern_to_string(tmp);					\
+	if (debug)														\
+	{																\
+		printf("=== case %s\n", input);								\
+		printf("actual   -> %s\n", actual);							\
+		printf("expected -> %s\n", expected);						\
+	}																\
+	mt_assert(strcmp(actual, expected) == 0);						\
+	pattern_del(tmp);												\
+	free(actual);													\
 }
 
-static void test_bracket(t_test *test)
-{
-    PATTERN_TEST("[]", "[]");
-    PATTERN_TEST("[\\[\\]]", "[[]]");
-    PATTERN_TEST("[[:digit:]]", "[[:digit:]]");
-    PATTERN_TEST("[a[:digit:]e[=a=]i[.ch.]o[:alnum:]u\\[\\]y]",
-                "[a[:digit:]e[=a=]i[.ch.]o[:alnum:]u[]y]");
-    PATTERN_TEST("[\"'\"]['\"']", "[\"'\"]['\"']");
-    PATTERN_TEST("Makefil[[:helo]]test", "Makefil[[:helo]]test");
-    PATTERN_TEST("*?k[AEIOU[:lower:]]**'f'\"i\"le", "*?k[AEIOU[:lower:]]**file");
-}
-
-static void test_simple(t_test *test)
-{
-    PATTERN_TEST("SALUT", "SALUT");
-    PATTERN_TEST("\\S\\A\\L\\U\\T", "SALUT");
-    PATTERN_TEST("S\\\\\\\\\\\\T", "S\\\\\\T");
-    PATTERN_TEST("'Salut'\"Salut\"", "SalutSalut");
-    PATTERN_TEST("\"\\\"\\\\\"", "\"\\");
-    PATTERN_TEST("*\"*\"*", "***");
-    PATTERN_TEST("?\"?\"?", "???");
-    PATTERN_TEST("*'*'*", "***");
-    PATTERN_TEST("?'?'?", "???");
-}
+mt_test_pattern_tostring(num1, "[]", "[]", false);
+mt_test_pattern_tostring(num2, "[\\[\\]]", "[[]]", false);
+mt_test_pattern_tostring(num3, "[[:digit:]]", "[[:digit:]]", false);
+mt_test_pattern_tostring(num4, "[a[:digit:]e[=a=]i[.ch.]o[:alnum:]u\\[\\]y]", "[a[:digit:]e[=a=]i[.ch.]o[:alnum:]u[]y]", false);
+mt_test_pattern_tostring(num5, "[\"'\"]['\"']", "[\"'\"]['\"']", false);
+mt_test_pattern_tostring(num6, "Makefil[[:helo]]test", "Makefil[[:helo]]test", false);
+mt_test_pattern_tostring(num7, "*?k[AEIOU[:lower:]]**'f'\"i\"le", "*?k[AEIOU[:lower:]]**file", false);
+mt_test_pattern_tostring(num8, "SALUT", "SALUT", false);
+mt_test_pattern_tostring(num9, "\\S\\A\\L\\U\\T", "SALUT", false);
+mt_test_pattern_tostring(num10, "S\\\\\\\\\\\\T", "S\\\\\\T", false);
+mt_test_pattern_tostring(num11, "'Salut'\"Salut\"", "SalutSalut", false);
+mt_test_pattern_tostring(num12, "\"\\\"\\\\\"", "\"\\", false);
+mt_test_pattern_tostring(num13, "*\"*\"*", "***", false);
+mt_test_pattern_tostring(num14, "?\"?\"?", "???", false);
+mt_test_pattern_tostring(num15, "*'*'*", "***", false);
+mt_test_pattern_tostring(num16, "?'?'?", "???", false);
 
 void        suite_pattern_to_string(t_suite *suite)
 {
-    SUITE_ADD_TEST(suite, test_simple);
-    SUITE_ADD_TEST(suite, test_bracket);
+	SUITE_ADD_TEST(suite, test_num1);
+	SUITE_ADD_TEST(suite, test_num2);
+	SUITE_ADD_TEST(suite, test_num3);
+	SUITE_ADD_TEST(suite, test_num4);
+	SUITE_ADD_TEST(suite, test_num5);
+	SUITE_ADD_TEST(suite, test_num6);
+	SUITE_ADD_TEST(suite, test_num7);
+	SUITE_ADD_TEST(suite, test_num8);
+	SUITE_ADD_TEST(suite, test_num9);
+	SUITE_ADD_TEST(suite, test_num10);
+	SUITE_ADD_TEST(suite, test_num11);
+	SUITE_ADD_TEST(suite, test_num12);
+	SUITE_ADD_TEST(suite, test_num13);
+	SUITE_ADD_TEST(suite, test_num14);
+	SUITE_ADD_TEST(suite, test_num15);
+	SUITE_ADD_TEST(suite, test_num16);
 }
