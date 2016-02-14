894e8e2593aec06d2bc23622e3e02d9393aeca32
yyang42 <yaoyanyang42@gmail.com>
2016-02-14 11:57:55 +0100

    fix/token_mgr_print: refactor diff tests

--- tests/ast_diff_tests/ast_diff_tests.sh
+++ tests/ast_diff_tests/ast_diff_tests.sh
@@ -60,6 +60,7 @@ then
         if [ -d "${CASE_PATH}" ]; then
             for TEST_PATH in $CASE_PATH/*; do
                 if [ -d "${TEST_PATH}" ]; then
+                    # mv $TESTS_ROOT_PATH/`basename $CASE_PATH`/`basename $TEST_PATH` $TESTS_ROOT_PATH/`basename $CASE_PATH`/`basename $TEST_PATH`_spec
                     # echo diff_test `basename $CASE_PATH` `basename $TEST_PATH`
                     diff_test `basename $CASE_PATH` `basename $TEST_PATH`
                 fi
