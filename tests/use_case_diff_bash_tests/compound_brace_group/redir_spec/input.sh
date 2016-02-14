171eff9dba450f0b14ec7ca06396b1aaffa7b333
yyang42 <yaoyanyang42@gmail.com>
2016-02-14 15:50:24 +0100

    feat/compound_commands_exec: fix stderr test

--- tests/use_case_diff_bash_tests/compound_brace_group/redir_spec/input.sh
+++ tests/use_case_diff_bash_tests/compound_brace_group/redir_spec/input.sh
@@ -1,4 +1,4 @@
-{ echo 123 ; echo abc; ls xxxxxx ; } > /tmp/test_redir_stdout 2> /tmp/test_redir_stderr
+{ echo 123 ; echo abc; >&2 echo "error" ; } > /tmp/test_redir_stdout 2> /tmp/test_redir_stderr
 echo --- stdout
 cat /tmp/test_redir_stdout
 echo --- stderr
