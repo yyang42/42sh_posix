3604d3a47f3eb491c70cae148eeec432b7cf2f12
Ludovic <lchenut@student.42.fr>
2016-03-01 17:29:23 +0100

    arithmetic expansion: added tests building ast error

--- tests/unit/tests/arexp/build_ast/arexp_build_error.spec.c
+++ tests/unit/tests/arexp/build_ast/arexp_build_error.spec.c
@@ -20,6 +20,12 @@ mt_test_arexp_error(num01, "1 +", "expected operand got nothing", false);
 mt_test_arexp_error(num02, "1 1", "unexpected token: 1", false);
 mt_test_arexp_error(num03, "1?1,B,c", "expected `:' for conditional expression, got nothing", false);
 mt_test_arexp_error(num04, "1&&+=0", "expected operand got +=", false);
+mt_test_arexp_error(num05, "12##13", "invalid number: 12##13", false);
+mt_test_arexp_error(num06, "(1+1:)", "expected `)' got :", false);
+mt_test_arexp_error(num07, "123456789a", "value too great for base: 123456789a", false);
+mt_test_arexp_error(num08, "37#abcDEF", "value too great for base: 37#abcDEF", false);
+mt_test_arexp_error(num09, "(1,2,)", "expected operand got )", false);
+mt_test_arexp_error(num10, "1,2,3?4,5,6:(7,8,9", "expected `)' got nothing", false);
 
 void suite_arexp_build_error(t_suite *suite)
 {
@@ -27,4 +33,10 @@ void suite_arexp_build_error(t_suite *suite)
 	SUITE_ADD_TEST(suite, test_num02);
 	SUITE_ADD_TEST(suite, test_num03);
 	SUITE_ADD_TEST(suite, test_num04);
+	SUITE_ADD_TEST(suite, test_num05);
+	SUITE_ADD_TEST(suite, test_num06);
+	SUITE_ADD_TEST(suite, test_num07);
+	SUITE_ADD_TEST(suite, test_num08);
+	SUITE_ADD_TEST(suite, test_num09);
+	SUITE_ADD_TEST(suite, test_num10);
 }
