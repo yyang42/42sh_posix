401ae24ecc9766384d889c2b92b8aaed9906f9fe
Ludovic <lchenut@student.42.fr>
2016-03-02 11:44:33 +0100

    arithmetic expansion: added tests

--- tests/unit/tests/arexp/eval/arexp_eval_middle.spec.c
+++ tests/unit/tests/arexp/eval/arexp_eval_middle.spec.c
@@ -20,23 +20,23 @@ static void test_## name(t_test *test)						\
 	arexp_del(arexp);										\
 }
 
-mt_test_eval(num01, "1+1", 2LL, false);
-mt_test_eval(num02, "1+1+1+1+1", 5LL, false);
-mt_test_eval(num03, "2*2", 4LL, false);
-mt_test_eval(num04, "2*2+2*2", 8LL, false);
-mt_test_eval(num05, "2*2*2*2", 16LL, false);
-mt_test_eval(num06, "2^3", 1LL, false);
-mt_test_eval(num07, "~35", -36LL, false);
-mt_test_eval(num08, "(((12)))", 12LL, false);
-mt_test_eval(num09, "-(((12)))", -12LL, false);
-mt_test_eval(num10, "33*77%41", 40LL, false);
-mt_test_eval(num11, " ! ! 1664", 1LL, false);
-mt_test_eval(num12, "~~--++--++--++42", 42LL, false);
-mt_test_eval(num13, "1*2*3*(4*5*6*7*8*9*(--10))", 3628800LL, false);
-mt_test_eval(num14, "9223372036854775807", LLONG_MAX, false);
-mt_test_eval(num15, "--9223372036854775808", LLONG_MIN, false);
+mt_test_eval(num01, "1+1-1-1?1,2,3:3?2:1", 2LL, false);
+mt_test_eval(num02, "1<=3^3&&12>42", 0LL, false);
+mt_test_eval(num03, "(12>3&&-7<0)?7<<32#10:55#33", 30064771072LL, false);
+mt_test_eval(num04, "2+2*2<<2*2+3>>1", 384LL, false);
+mt_test_eval(num05, "10>>-1", 0LL, false);
+mt_test_eval(num06, "(2+2*2<<2*2+3>>-1)+1", 1LL, false);
+mt_test_eval(num07, "(~((55*33)-(45>>4)))*-+077", 114282LL, false);
+mt_test_eval(num08, "0x10+0X10+010+10+2#10", 52LL, false);
+mt_test_eval(num09, "-(-(-(1<<1)-2)*3)", -12LL, false);
+mt_test_eval(num10, "12<33>0<=1", 1LL, false);
+mt_test_eval(num11, "12!=12==0?~~12:-12", 12LL, false);
+mt_test_eval(num12, "63#@*64#_", 3906LL, false);
+mt_test_eval(num13, "(144&335|667)^2#10010011101110", 9845LL, false);
+mt_test_eval(num14, "-00+0<<0*0^0|0&0*0-0", 0LL, false);
+mt_test_eval(num15, "(4456333<(-32, 33, 00033333333) && (1, 22, 333))? 44==45? 12? 12?13: 14!=33 ? 12 : 33: 666*2<<5:(10+1)*(1<<2)-2#10 : 64#lol", 42LL, false);
 
-void suite_arexp_eval_simple(t_suite *suite)
+void suite_arexp_eval_middle(t_suite *suite)
 {
 	SUITE_ADD_TEST(suite, test_num01);
 	SUITE_ADD_TEST(suite, test_num02);
