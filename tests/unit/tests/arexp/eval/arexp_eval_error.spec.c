15f85dd76f46b03b859062003261b043b429560b
Ludovic <lchenut@student.42.fr>
2016-03-03 17:43:15 +0100

    arithmetic expansion: added one more file of test

---
+++ tests/unit/tests/arexp/eval/arexp_eval_error.spec.c
@@ -0,0 +1,37 @@
+#include <project.h>
+
+#include "arexp/arexp.h"
+#include <limits.h>
+#include "environment.h"
+
+# define mt_test_eval(name, input, expected, debug)			\
+static void test_## name(t_test *test)						\
+{															\
+	t_arexp		*arexp = arexp_new(input);					\
+	arexp_eval(arexp);										\
+	char		*actual = arexp->error_msg;					\
+	if (debug)												\
+	{														\
+		printf("=== case %s\n", input);						\
+		printf("actual   {%s}\n", actual);					\
+		printf("expected {%s}\n", expected);				\
+	}														\
+	mt_assert(strcmp(actual, expected) == 0);				\
+	arexp_del(arexp);										\
+}
+
+mt_test_eval(num01, "1/0", "division by 0", false);
+mt_test_eval(num02, "1%0", "division by 0", false);
+mt_test_eval(num03, "a", "value too great for base: Bonjour", false);
+mt_test_eval(num04, "a=100, a/(a>>10)", "division by 0", false);
+mt_test_eval(num05, "a=100, a%(a>>10)", "division by 0", false);
+
+void suite_arexp_eval_error(t_suite *suite)
+{
+	SUITE_ADD_TEST(suite, test_num01);
+	SUITE_ADD_TEST(suite, test_num02);
+	environment_setenv_value(environment_singleton(), "a", "Bonjour", 1);
+	SUITE_ADD_TEST(suite, test_num03);
+	SUITE_ADD_TEST(suite, test_num04);
+	SUITE_ADD_TEST(suite, test_num05);
+}
