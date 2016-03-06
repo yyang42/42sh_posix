5c356206eb35942dd5fe9df99a54bad35176e141
Ludovic CHENUT <chenut.ludovic@gmail.com>
2016-03-06 10:20:10 +0100

    builtin read: begin

---
+++ includes/builtin/builtin_read.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_norris_loves_the_norminette.c                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chuck <chuck@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2042/02/30 42:00:00 by chuck             #+#    #+#             */
+/*   Updated: 2042/02/30 41:59:59 by chuck            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUILTIN_READ_H
+# define BUILTIN_READ_H
+
+# include "basics.h"
+# include "twl_opt.h"
+# include "builtin/builtin.h"
+
+int					builtin_read_exec(t_lst *tokens, t_environment *this);
+
+#endif
