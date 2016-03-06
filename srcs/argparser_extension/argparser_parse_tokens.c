a9aadb5f0ba05d6555841c64cbab7341ab9710b0
yyang42 <yaoyanyang42@gmail.com>
2016-03-06 19:21:48 +0100

    feat/refactor_execution: refactor export

--- srcs/argparser_extension/argparser_parse_tokens.c
+++ srcs/argparser_extension/argparser_parse_tokens.c
@@ -11,38 +11,15 @@
 /* ************************************************************************** */
 
 #include "argparser_extension.h"
+#include "token/token_mgr.h"
 
-#include "builtin/cmds/builtin_export.h"
-
-t_argparser			*builtin_export_argparser(void)
+t_argparser_result	*argparser_parse_tokens(t_argparser *this, t_lst *ast_tokens)
 {
-	static t_argparser		*argparser;
+	t_argparser_result	*result;
+	t_lst				*str_tokens;
 
-	if (argparser == NULL)
-	{
-		argparser = argparser_new("export");
-		argparser_set_usage_extra(argparser, " [name[=value] ...]");
-		argparser_add_argument(argparser, argparser_argument_new('p', NULL, "Print all exported variables", 0));
-	}
-	return (argparser);
-}
-
-
-void				builtin_export_exec(t_lst *tokens, t_shenv *env)
-{
-	t_argparser_result *argparser_result;
-
-	argparser_result = argparser_parse_tokens(builtin_export_argparser(), tokens);
-	if (argparser_result->err_msg)
-	{
-		argparser_result_print_error_with_help(argparser_result);
-	}
-	else
-	{
-		if (argparser_result_opt_is_set(argparser_result, "p"))
-			builtin_export_verbose(env);
-		else
-			builtin_export_add(env, tokens);
-	}
-	shenv_set_last_exit_status(env, BUILTIN_EXEC_SUCCESS);
+	str_tokens = token_mgr_to_lst(ast_tokens);
+	result = argparser_parse(this, str_tokens);
+	twl_lst_del(str_tokens, NULL);
+	return (result);
 }
