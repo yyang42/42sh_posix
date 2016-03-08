a49ea60cca385867c120265283f4a6048f184d95
yyang42 <yaoyanyang42@gmail.com>
2016-03-08 09:26:50 +0100

    feat/execution: shenv_shvars_set_equal to shenv_shvars_set_split_by_equal

--- srcs/builtin/cmds/builtin_export/builtin_export_exec_export_token.c
+++ srcs/builtin/cmds/builtin_export/builtin_export_exec_export_token.c
@@ -19,7 +19,7 @@
 
 void				iter_fn(void *str_token, void *shenv, void *command_name)
 {
-	shenv_shvars_set_equal(shenv, str_token, command_name);
+	shenv_shvars_set_split_by_equal(shenv, str_token, command_name);
 }
 
 void				builtin_export_exec_export_tokens(t_argparser_result *argparser_result, t_shenv *shenv)
