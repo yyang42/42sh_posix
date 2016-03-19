d22d1a5629807f696cda65f49345f229e223b85e
yyang42 <yaoyanyang42@gmail.com>
2016-03-19 20:01:43 +0100

    feat/async_list: kill error handling 2

--- tests/use_case_diff_bash_tests/builtin_kill/kill_terminated_spec/input.sh
+++ tests/use_case_diff_bash_tests/builtin_kill/kill_terminated_spec/input.sh
@@ -4,6 +4,6 @@
 	sleep 0.2 &
 	jobs
 	/bin/kill $pid
-	sleep 0.1
+	sleep 0.2
 	jobs -l
-) 2>&1 | sed -E 's/[0-9]+/X/g' | sed -e 's/.*input\.sh/CMD_PATH/g' | sed -e 's/ &//g' | sort
+) 2>&1 | sed -E 's/[0-9]+/X/g' | sed -e 's/.*input\.sh/CMD_PATH/g' | sed -e 's/ &//g' | sort | cat -e
