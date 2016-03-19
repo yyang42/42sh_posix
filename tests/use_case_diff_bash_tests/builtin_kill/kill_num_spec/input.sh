b5bac911b148b25dc5866397e0f1ca2c64bfbae6
yyang42 <yaoyanyang42@gmail.com>
2016-03-19 19:36:12 +0100

    feat/async_list: try fix ci kill

--- tests/use_case_diff_bash_tests/builtin_kill/kill_num_spec/input.sh
+++ tests/use_case_diff_bash_tests/builtin_kill/kill_num_spec/input.sh
@@ -3,4 +3,4 @@
 	kill -31 $(jobs -p)
 	sleep 0.1
 	sleep 0.1
-} 2>&1 | sed -E 's/[0-9]+ Terminated/<PID> Terminated/g'
+} 2>&1 | sed -E 's/[0-9]+ Terminated/<PID> Terminated/g' | sed -e 's/.*input\.sh/<CMD_PATH>/g'
