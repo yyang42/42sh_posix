fbaa772a475d8b29bed5f577d18a5b140f21b19f
Ludovic <lchenut@student.42.fr>
2016-03-21 22:53:29 +0100

    expansion: added tokenizer tests

--- tests/unit/tests/expan/expan_tokenizer/expan_tokenizer_rule03.spec.c
+++ tests/unit/tests/expan/expan_tokenizer/expan_tokenizer_rule03.spec.c
@@ -7,6 +7,8 @@ mt_test_expan(num02, "''''", 0, "''", EXPAN_SQUOTE, false)
 mt_test_expan(num03, "''''", 1, "''", EXPAN_SQUOTE, false)
 mt_test_expan(num04, "'\\'''", 0, "'\\'", EXPAN_SQUOTE, false)
 mt_test_expan(num05, "'\\'''", 1, "''", EXPAN_SQUOTE, false)
+mt_test_expan(num06, "toto'$a${a}`ls` \"ohnoes\"'pouet", 1,
+								"'$a${a}`ls` \"ohnoes\"'", EXPAN_SQUOTE, false)
 
 void suite_expan_tokenizer_rule03(t_suite *suite)
 {
@@ -15,4 +17,5 @@ void suite_expan_tokenizer_rule03(t_suite *suite)
 	SUITE_ADD_TEST(suite, test_num03);
 	SUITE_ADD_TEST(suite, test_num04);
 	SUITE_ADD_TEST(suite, test_num05);
+	SUITE_ADD_TEST(suite, test_num06);
 }
