b8682875d557ebcaf53809a55ed65d5f8aea2fa1
yyang42 <yaoyanyang42@gmail.com>
2016-04-01 20:20:43 +0200

    feat/builtins: trap: fix tests

--- tests/use_case_diff_bash_tests/builtin_trap/trap_ignored_on_entry_42sh_spec/input.sh
+++ tests/use_case_diff_bash_tests/builtin_trap/trap_ignored_on_entry_42sh_spec/input.sh
@@ -1,8 +1,5 @@
 trap '' HUP INT USR1 USR2
 trap 'echo USR2msg' USR2
 trap
-bash --posix -c 'trap "echo abc" USR1 INT HUP  && trap | sort'
-bash --posix -c 'trap "echo USR2msgOverriden" USR2 && trap | sort'
-
 ./42sh -c 'trap "echo abc" USR1 INT HUP && trap | sort'
 ./42sh -c 'trap "echo USR2msgOverriden" USR2 && trap | sort'
