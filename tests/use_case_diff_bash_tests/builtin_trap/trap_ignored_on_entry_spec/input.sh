63d79760f86da4a16eb1d3e92102fc2c697cc993
yyang42 <yaoyanyang42@gmail.com>
2016-04-01 19:59:59 +0200

    feat/builtins: fix trap_ignored_on_entry_spec/expected_stdout 2

--- tests/use_case_diff_bash_tests/builtin_trap/trap_ignored_on_entry_spec/input.sh
+++ tests/use_case_diff_bash_tests/builtin_trap/trap_ignored_on_entry_spec/input.sh
@@ -1,8 +1,8 @@
 trap '' HUP INT USR1 USR2
 trap 'echo USR2msg' USR2
 trap
-bash -c 'trap "echo abc" USR1 INT HUP  && trap | sort'
-bash -c 'trap "echo USR2msgOverriden" USR2 && trap | sort'
+bash --posix -c 'trap "echo abc" USR1 INT HUP  && trap | sort'
+bash --posix -c 'trap "echo USR2msgOverriden" USR2 && trap | sort'
 
 ./42sh -c 'trap "echo abc" USR1 INT HUP && trap | sort'
 ./42sh -c 'trap "echo USR2msgOverriden" USR2 && trap | sort'
