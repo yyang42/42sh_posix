7c984bc446fe21fc8896fda1d0c82391ef43b5aa
yyang42 <yaoyanyang42@gmail.com>
2016-04-14 00:54:02 +0200

    feat/builtins: remove print in tests

--- tests/unit/tests/ast/ast_syntax_error_exec.spec.c
+++ tests/unit/tests/ast/ast_syntax_error_exec.spec.c
@@ -2,8 +2,8 @@
 
 #include "ast/ast.h"
 
-mt_test_ast_error(01, "abc (", "SyntaxError 1 : Syntax error near '('", true);
-mt_test_ast_error(02, "echo \"", "SyntaxError 1 : looking for matching `\"'", true);
+mt_test_ast_error(01, "abc (", "SyntaxError 1 : Syntax error near '('", false);
+mt_test_ast_error(02, "echo \"", "SyntaxError 1 : looking for matching `\"'", false);
 
 void	suite_ast_syntax_error_exec(t_suite *suite)
 {
