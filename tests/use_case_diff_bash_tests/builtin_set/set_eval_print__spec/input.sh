c10aba8f7d4d23390033502f34ae38401fa5046b
yyang42 <yaoyanyang42@gmail.com>
2016-04-15 23:26:07 +0200

    feat/builtin_set: set: test set print is executable

---
+++ tests/use_case_diff_bash_tests/builtin_set/set_eval_print__spec/input.sh
@@ -0,0 +1,17 @@
+test_var1='echo abc'
+test_var2='abc'
+test_var3="'"
+set | grep test_var= > /tmp/builtin_set_stdout
+
+echo "/tmp/builtin_set_stdout content:"
+cat /tmp/builtin_set_stdout
+unset test_var1
+
+echo "test_var1 is unset so grep should fail"
+set | grep test_var | grep -v _=
+set | grep test_var1=
+echo "grep exit status $?"
+
+echo "sourcing the stdout should set test_var1"
+. /tmp/builtin_set_stdout
+set | grep test_var1=
--- tests/use_case_diff_bash_tests/builtin_set/set_print_spec/input.sh
+++ tests/use_case_diff_bash_tests/builtin_set/set_print_spec/input.sh
@@ -1,15 +1,15 @@
-tes_set_newline='newline
+test_set_newline='newline
 
 bb'
-set | grep -A 2 tes_set_newline
+set | grep -A 2 test_set_newline
 
-tes_set_var0='simplestring'
-tes_set_var1='tabs		bb'
-tes_set_var2='dollar$'
-tes_set_var3='space bb'
-tes_set_var4="space bb"
-tes_set_var5="space_?^bb"
-tes_set_var6="space_bb"
-tes_set_var7="space1bb"
-tes_set_var7="space@bb"
-set | grep -A 2 tes_set_var
+test_set_var0='simplestring'
+test_set_var1='tabs		bb'
+test_set_var2='dollar$'
+test_set_var3='space bb'
+test_set_var4="space bb"
+test_set_var5="space_?^bb"
+test_set_var6="space_bb"
+test_set_var7="space1bb"
+test_set_var7="space@bb"
+set | grep -A 2 test_set_var
