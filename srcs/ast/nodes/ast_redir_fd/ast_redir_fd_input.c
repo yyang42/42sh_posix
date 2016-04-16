1d5dc2e054081fe5ae5062b549ed37f9202fef1d
yyang42 <yaoyanyang42@gmail.com>
2016-04-16 15:04:51 +0200

    fix/heredoc: add noci to heredoc test

--- srcs/ast/nodes/ast_redir_fd/ast_redir_fd_input.c
+++ srcs/ast/nodes/ast_redir_fd/ast_redir_fd_input.c
@@ -18,7 +18,7 @@ void	ast_redir_fd_redir_input(t_ast_redir *redir, t_ast_redir_fd *redir_fd)
 		? STDIN_FILENO : redir->io_number);
 	redir_fd->fd_origin = redir->io_number == -1
 		? STDIN_FILENO : redir->io_number;
-	if (!twl_strcmp("<", redir->operator))
+	if (twl_strequ("<", redir->operator))
 		redir_fd->fd_file = read_file(redir->param);
 	else if (ast_redir_utils_is_heredoc(redir->operator))
 		redir_fd->fd_file = ast_redir_fd_write_heredoc_to_tmp_file(redir);
