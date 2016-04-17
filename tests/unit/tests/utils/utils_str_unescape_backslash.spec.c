91fe008408dfa52c4fe42e4a2179641b499527b3
yyang42 <yaoyanyang42@gmail.com>
2016-04-17 19:36:23 +0200

    feat/builtin_read: better escape handling 2

---
+++ tests/unit/tests/utils/utils_str_unescape_backslash.spec.c
@@ -0,0 +1,37 @@
+#include <project.h>
+
+#include "utils.h"
+
+# define mt_test_utils_str_unescape_backslash(test_name, input, expected, debug) \
+	static void test_name(t_test *test) \
+	{ \
+		char			*actual; \
+		actual = utils_str_unescape_backslash(input); \
+		if (debug) \
+		{ \
+			printf("==== case : %s\n", input); \
+			printf("actual    {%s}\n", actual); \
+			printf("expected  {%s}\n", expected); \
+		} \
+		mt_assert(strcmp(actual, expected) == 0); \
+		free(actual); \
+	}
+
+mt_test_utils_str_unescape_backslash(test, "ab", "ab", false);
+mt_test_utils_str_unescape_backslash(test1, "aa\\bb", "aabb", false);
+mt_test_utils_str_unescape_backslash(test2, "aa\\\\bb", "aa\\bb", false);
+mt_test_utils_str_unescape_backslash(test3, "\\", "", false);
+mt_test_utils_str_unescape_backslash(test4, "\\\\", "\\", false);
+mt_test_utils_str_unescape_backslash(test5, "", "", false);
+mt_test_utils_str_unescape_backslash(test6, "\\\n", "\n", false);
+
+void	suite_utils_str_unescape_backslash(t_suite *suite)
+{
+	SUITE_ADD_TEST(suite, test);
+	SUITE_ADD_TEST(suite, test1);
+	SUITE_ADD_TEST(suite, test2);
+	SUITE_ADD_TEST(suite, test3);
+	SUITE_ADD_TEST(suite, test4);
+	SUITE_ADD_TEST(suite, test5);
+	SUITE_ADD_TEST(suite, test6);
+}
