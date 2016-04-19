93a33944ed5cdb5838ad1aedaf2ffbec61af5948
yyang42 <yaoyanyang42@gmail.com>
2016-04-19 20:33:43 +0200

    fix/bugs: return: fix tests

---
+++ tests/use_case_diff_bash_tests/42sh/42sh_input_from_stdin_spec_NOK/input.sh
@@ -0,0 +1,1 @@
+echo "echo abc" | $TESTED_SHELL
--- tests/use_case_diff_bash_tests/42sh_self/42sh_cat_spec/input.sh
+++
@@ -1,4 +0,0 @@
-echo "one cat"
-echo abcde | ./42sh -c 'cat -e'
-echo "many cats"
-echo abcde | ./42sh -c 'cat | cat -e | cat -e | cat -e | cat -e | cat -e | cat -e | cat -e | cat -e | cat -e | cat -e | cat -e | cat -e | cat -e | cat -e | cat -e | cat -e | cat -e | cat -e | cat -e'
