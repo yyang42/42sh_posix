567cd60c8389724481619afcabec3614edc0dca0
yyang42 <yaoyanyang42@gmail.com>
2016-04-19 23:03:33 +0200

    fix/bugs: handle opt err

--- srcs/prog/prog_init.c
+++ srcs/prog/prog_init.c
@@ -30,4 +30,9 @@ void				prog_init(t_prog *prog, char **argv)
 {
 	prog->argparser = init_argparser();
 	prog->argparser_result = argparser_parse_from_arr(prog->argparser, argv);
+	if (prog->argparser_result)
+	{
+		argparser_result_print_error_with_help(prog->argparser_result);
+		exit(1);
+	}
 }
