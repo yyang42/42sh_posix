6f13b68eba6635c799b2067ff3f57638a6ba6720
yyang42 <yaoyanyang42@gmail.com>
2016-04-19 20:43:06 +0200

    fix/bugs: return: fix tests 2

--- tests/use_case_diff_bash_tests/builtin_return/return_overflow_spec/input.sh
+++ tests/use_case_diff_bash_tests/builtin_return/return_overflow_spec/input.sh
@@ -3,17 +3,24 @@ myfun () {
 	return $1;
 	echo end
 }
-myfun 42
-echo $?
-myfun 1
-echo $?
-myfun 0
-echo $?
-myfun -1
-echo $?
-myfun 254
-echo $?
-myfun 255
-echo $?
-myfun 256
-echo $?
+
+{
+	myfun 42
+	echo $?
+	myfun 1
+	echo $?
+	myfun 0
+	echo $?
+	myfun -1
+	echo $?
+	myfun 254
+	echo $?
+	myfun 255
+	echo $?
+	myfun 256
+	myfun 1000
+	echo $?
+} >/tmp/test_builtin_return_stdout 2> /tmp/test_builtin_return_stderr
+
+cat /tmp/test_builtin_return_stdout | sed -e 's/.*input\.sh/CMD_PATH/g'
+cat /tmp/test_builtin_return_stderr | sed -e 's/.*input\.sh/CMD_PATH/g'
