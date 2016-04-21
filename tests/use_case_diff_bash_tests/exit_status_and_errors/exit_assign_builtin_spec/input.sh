83cef7f19d4133da150e1c1ba104af2127d28d83
yyang42 <yaoyanyang42@gmail.com>
2016-04-21 19:29:46 +0200

    fix/bugs: exit: more assign tests

--- tests/use_case_diff_bash_tests/exit_status_and_errors/exit_assign_builtin_spec/input.sh
+++ tests/use_case_diff_bash_tests/exit_status_and_errors/exit_assign_builtin_spec/input.sh
@@ -6,7 +6,7 @@ echo "=== test $cmd"
 cat << EOF > /tmp/test_exit_builtin_file
 echo "before"
 readonly myvar
-myvar=bb $cmd
+myvar=bb $cmd > /dev/null
 echo "after"
 EOF
 
@@ -17,7 +17,28 @@ echo
 foo .
 foo :
 foo alias
+# foo bg
 foo break
+foo cd
 foo continue
+foo echo
 foo env
 foo eval
+foo export
+foo false
+# foo fg
+foo jobs
+# foo kill
+# foo read
+foo readonly
+foo return
+foo set
+# foo setenv
+foo shift
+foo times
+foo trap
+foo true
+foo umask
+foo unset
+# foo unsetenv
+foo wait
