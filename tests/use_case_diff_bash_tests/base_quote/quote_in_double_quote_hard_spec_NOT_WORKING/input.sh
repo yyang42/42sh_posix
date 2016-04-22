c65ceda0203847170d3bf2c17478882fa0f1bd55
yyang42 <yaoyanyang42@gmail.com>
2016-04-22 20:31:12 +0200

    fix/bugs: add non working tests for expan

---
+++ tests/use_case_diff_bash_tests/base_quote/quote_in_double_quote_hard_spec_NOT_WORKING/input.sh
@@ -0,0 +1,4 @@
+echo $( echo 1a" 2a2b $( echo 3a" 3b' "3c ) 2c'''2d "1b )
+echo $( echo 1a" 2a'2b $( echo 3a" 3b' "3c ) 2c'''2d "1b )
+echo $( echo 1a" 2a''2b $( echo 3a" 3b' "3c ) 2c'''2d "1b )
+echo $( echo 1a" 2a'''2b $( echo 3a" 3b' "3c ) 2c'''2d "1b )
