d30bf218137e4dc0052f4b05628d0d94a9191790
yyang42 <yaoyanyang42@gmail.com>
2016-04-24 22:15:41 +0200

    fix/bugs_and_refactor: noci tests

--- tests/use_case_diff_bash_tests/builtin_env/env_alias_noci_spec/input.sh
+++ tests/use_case_diff_bash_tests/builtin_env/env_alias_noci_spec/input.sh
@@ -2,5 +2,5 @@ alias aa=bb
 echo ">> 1"
 alias
 echo ">> 2"
-env alias 2>&1 | sed 's/env:/42sh: line 1:/g'
+env alias
 echo ">> 3"
--- tests/use_case_diff_bash_tests/builtin_env/env_err_cmd_err_spec/input.sh
+++
@@ -1,1 +0,0 @@
-env cd folder_not_exist 2>&1 | sed 's/env:/42sh: line 1:/g'
