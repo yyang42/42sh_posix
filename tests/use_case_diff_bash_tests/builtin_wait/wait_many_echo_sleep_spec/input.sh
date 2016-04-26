f7658957627bd1cb5e58deb37d25a87d70c383f5
yyang42 <yaoyanyang42@gmail.com>
2016-04-26 21:17:20 +0200

    fix/bugs_and_refactor: add wait tests 2

--- tests/use_case_diff_bash_tests/builtin_wait/wait_many_echo_sleep_spec/input.sh
+++ tests/use_case_diff_bash_tests/builtin_wait/wait_many_echo_sleep_spec/input.sh
@@ -1,5 +1,4 @@
-set -m
-sleep 0.01 && echo "sleep 1" && \
+sleep 0.05 && echo "sleep 1" && \
 sleep 0.01 && echo "sleep 2" && \
 sleep 0.01 && echo "sleep 3" && \
 sleep 0.01 && echo "sleep 4" && \
@@ -15,5 +14,6 @@ sleep 0.01 && echo "sleep 13" && \
 sleep 0.01 && echo "sleep 14" && \
 sleep 0.01 && echo "sleep 15" && \
 sleep 0.01 && echo "sleep 16" &
-fg > /tmp/builtin_fg_test_file && cat -e /tmp/builtin_fg_test_file | sed 's/ &//g'
+echo "before wait"
 wait
+echo "after wait"
---
+++ tests/use_case_diff_bash_tests/builtin_wait/wait_simple_echo_spec/input.sh
@@ -0,0 +1,4 @@
+sleep 0.1 && echo "sleep 16" &
+echo "before wait"
+wait
+echo "after wait"
