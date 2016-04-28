38e5f70f9c17dd119f479b691e1926b13ff47b06
Ludovic <lchenut@student.42.fr>
2016-04-28 12:27:25 +0200

    command builtin: command + builtin done

--- srcs/builtin/cmds/builtin_command/builtin_command_exec_command.c
+++ srcs/builtin/cmds/builtin_command/builtin_command_exec_command.c
@@ -14,15 +14,14 @@
 #include "builtin/builtin_mgr.h"
 #include "data.h"
 
-//static void		wrapper_iter_fn(void *data, void *ctx1, void *ctx2)
-//{
-//	t_token		*new;
-//
-//	new = token_copy(ctx2);
-//	token_set_text(new, data);
-//	twl_printf("%s\n", data);
-//	twl_lst_push_back(ctx1, new);
-//}
+static void		wrapper_iter_fn(void *data, void *ctx1, void *ctx2)
+{
+	t_token		*new;
+
+	new = token_copy(ctx2);
+	token_set_text(new, data);
+	twl_lst_push_back(ctx1, new);
+}
 
 void			builtin_command_exec_command(t_lst *tokens,
 												t_argparser_result *result,
@@ -30,16 +29,19 @@ void			builtin_command_exec_command(t_lst *tokens,
 {
 	t_builtin	*builtin;
 	char		*cmd_name;
+	t_lst		*wrapper;
 
-	//wrapper = twl_lst_new();
-	//twl_lst_iter2(result->remainders, wrapper_iter_fn, wrapper,
-	//													twl_lst_first(tokens));
 	cmd_name = twl_lst_first(result->remainders);
-	(void)tokens;
-	//twl_printf("->%s<-\n", cmd_name);
 	builtin = builtin_mgr_find_by_name(data_builtins(), cmd_name);
 	if (builtin)
-		builtin->builtin_fn(result->remainders, shenv);
+	{
+		wrapper = twl_lst_new();
+		twl_lst_iter2(result->remainders, wrapper_iter_fn, wrapper,
+														twl_lst_first(tokens));
+		builtin->builtin_fn(wrapper, shenv);
+	}
 	else
+	{
 		ast_simple_command_execve(result->remainders, result->remainders);
+	}
 }
