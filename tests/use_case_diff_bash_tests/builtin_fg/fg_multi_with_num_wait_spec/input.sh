e56437f33b903b516cf87143364a93dffdeb4c7a
yyang42 <yaoyanyang42@gmail.com>
2016-04-30 21:39:33 +0200

    feat/job_control: add sleep to fg tests

--- tests/use_case_diff_bash_tests/builtin_fg/fg_multi_with_num_wait_spec/input.sh
+++ tests/use_case_diff_bash_tests/builtin_fg/fg_multi_with_num_wait_spec/input.sh
@@ -1,6 +1,7 @@
 set -m
-sleep 0.1 && echo "background msg3" &
-sleep 0.2 && echo "background msg4" &
+sleep 0.2 && echo "background msg3" &
+sleep 0.3 && echo "background msg4" &
+sleep 0.1
 fg %2 > /tmp/builtin_fg_test_file && cat -e /tmp/builtin_fg_test_file | sed 's/ &//g'
 fg %1 > /tmp/builtin_fg_test_file && cat -e /tmp/builtin_fg_test_file | sed 's/ &//g'
 wait
