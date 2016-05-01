f86c71f04bfd9b14ae4fdd6ba20a9ac1652eb4d6
Ludovic CHENUT <chenut.ludovic@gmail.com>
2016-05-01 14:19:56 +0200

    builtin command: added tests for if clause format

---
+++ tests/use_case_diff_bash_tests/builtin_command/true_verbose_if_clause_spec/input.sh
@@ -0,0 +1,18 @@
+lel ()
+{
+	if ( abc + bde = lel )
+	then
+		! echo 'Oh !'
+	elif ls -la
+	then
+		printf "Eh ouais coco"
+	elif cat file??? | grep -oP 'lel'
+	then
+		printf "Eh ouais coco"
+	elif i=12; for i in `seq 1 666`; do echo pouet; done
+	then
+		printf "Eh ouais coco"
+	fi > lel
+}
+
+command -V lel
