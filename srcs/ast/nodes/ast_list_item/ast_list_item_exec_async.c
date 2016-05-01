99586d05702df974aadd93b20093cad0e20a6e72
yyang42 <yaoyanyang42@gmail.com>
2016-05-01 11:51:56 +0200

    feat/job_control: fix $!

--- srcs/ast/nodes/ast_list_item/ast_list_item_exec_async.c
+++ srcs/ast/nodes/ast_list_item/ast_list_item_exec_async.c
@@ -31,6 +31,7 @@ static void			wait_fn(int pid, void *this_)
 	LOG_INFO("ast_list_item_exec_async: wait_fn");
 	str_tokens = token_mgr_to_lst(this->list_item_tokens);
 	job_mgr_env_push(job_new(pid, str_tokens));
+	shenv_singleton()->info.most_recent_background_command_pid = pid;
 	twl_lst_del(str_tokens, NULL);
 }
 
