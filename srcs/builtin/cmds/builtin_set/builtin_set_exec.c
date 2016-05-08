86fd50c3a997941eba9d3b94c6f3c20f1a370cd7
yyang42 <yaoyanyang42@gmail.com>
2016-05-08 19:48:13 +0200

    feat/builtin_set: refactor builtin set

--- srcs/builtin/cmds/builtin_set/builtin_set_exec.c
+++ srcs/builtin/cmds/builtin_set/builtin_set_exec.c
@@ -79,13 +79,18 @@ void				builtin_set_exec(t_lst *tokens, t_shenv *env)
 	if (twl_lst_len(tokens_copy) == 0)
 	{
 		builtin_set_exec_print(env);
-		return ;
 	}
-	if (twl_strequ(token_mgr_get(tokens_copy, 0)->text, "--"))
+	else
 	{
-		shenv_remove_all_pos_params(env);
-		if (twl_lst_len(tokens_copy) == 1)
-			return ;
+		if (twl_strequ(token_mgr_get(tokens_copy, 0)->text, "--"))
+		{
+			shenv_remove_all_pos_params(env);
+			if (twl_lst_len(tokens_copy) == 1)
+			{
+				twl_lst_del(tokens_copy, NULL);
+				return ;
+			}
+		}
 	}
 	builtin_set_exec_args(tokens_copy, env);
 	twl_lst_del(tokens_copy, NULL);
