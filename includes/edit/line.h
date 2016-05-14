4597aaf395f671fe7b3503869affe0ff62e8f6f0
Ludovic CHENUT <chenut.ludovic@gmail.com>
2016-05-14 12:53:30 +0200

    line edition: build preview structure

---
+++ includes/edit/line.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_norris_loves_the_norminette.c                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chuck <chuck@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2042/02/30 42:00:00 by chuck             #+#    #+#             */
+/*   Updated: 2042/02/30 41:59:59 by chuck            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LINE_H
+# define LINE_H
+
+# include "basics.h"
+
+# define DFL_LINE_SIZE 64
+
+typedef struct			s_line
+{
+	char				*line;
+	size_t				total;
+	size_t				size;
+}						t_line;
+
+t_line					*line_new(void);
+void					line_del(t_line *this);
+
+char					*line_get(t_line *this);
+
+#endif
---
+++ srcs/edit/edit/edit_singleton.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_norris_loves_the_norminette.c                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chuck <chuck@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2042/02/30 42:00:00 by chuck             #+#    #+#             */
+/*   Updated: 2042/02/30 41:59:59 by chuck            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "edit/edit.h"
+
+t_edit				*edit_singleton(void)
+{
+	static t_edit	*this = NULL;
+
+	if (!this)
+	{
+		this = edit_new();
+	}
+	return (this);
+}
