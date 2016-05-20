4ff9434c5a161c05f306fdd110a77303c9ad54dd
yyang42 <yaoyanyang42@gmail.com>
2016-05-20 22:15:31 +0200

    feat/set_and_sh: heredoc: hard test

--- tests/use_case_diff_bash_tests/base_redirs_heredoc/redir_heredoc_cmd_sub_same_line_hard_spec/input.sh
+++ tests/use_case_diff_bash_tests/base_redirs_heredoc/redir_heredoc_cmd_sub_same_line_hard_spec/input.sh
@@ -2,7 +2,23 @@ cat << EOF1; echo $( cat << EOF2; echo $( cat << EOF3;
 a1
 a2
 EOF3
-) | cat -e | wc; cat -e <<- EOF4
+) | cat -e | wc; cat <<- EOF4 | cat -e; echo `cat <<- EOF5; printf "$( cat << EOF6
+BB
+AA
+$(cat << EOF7
+$(cat << EOF8
+DD
+EOF8
+)
+EOF7
+)
+CC
+EOF6
+)" | sort
+	tab eeeee1
+		tab x2 eeeee2
+EOF5
+`
 bb1
 bb2
 EOF2
