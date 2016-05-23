13a0ef553191c985ac44a0a52221a3450c5bc8fb
yyang42 <yaoyanyang42@gmail.com>
2016-05-23 19:59:00 +0200

    fix/bugs: fix tests

--- tests/use_case_diff_bash_tests/compound_subshell/subshell_complex_redir_spec/input.sh
+++ tests/use_case_diff_bash_tests/compound_subshell/subshell_complex_redir_spec/input.sh
@@ -1,9 +1,6 @@
 (
-	# exec 43<&0 # valid
 	echo '======1'
 	exec 43<&42 # 42 not valid
 	echo '======2'
-	# exec 7777<&0 # 7777 not valid
-	# echo '======3'
 ) &> /tmp/builtin_exec_stdrr
 cat /tmp/builtin_exec_stdrr | sed -e 's/.*input\.sh/CMD_PATH/g'
