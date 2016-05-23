a2ea0abe816859c6e9a3ca9ce5469a02fc917685
yyang42 <yaoyanyang42@gmail.com>
2016-05-23 20:22:34 +0200

    fix/bugs: read not printf if empty 2

--- tests/use_case_diff_bash_tests/builtin_read/read_long_line_missing_var_spec/input.sh
+++ tests/use_case_diff_bash_tests/builtin_read/read_long_line_missing_var_spec/input.sh
@@ -2,5 +2,7 @@ python -c 'print "aabbcc\n"*45' > /tmp/builtin_read_file
 
 while read -r xx yy
 do
-    printf "%s %s\n" "$yy" "$xx"
+	if [ ! -z "$yy" ]; then
+    	printf "%s %s\n" "$yy" "$xx"
+	fi
 done < /tmp/builtin_read_file
