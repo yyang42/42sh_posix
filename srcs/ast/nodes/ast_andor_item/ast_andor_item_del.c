1e08490ef2373533c1d74ba2077e22caf21a31a5
yyang42 <yaoyanyang42@gmail.com>
2016-05-26 19:26:54 +0200

    fix/leaks: fix leaks tests/use_case_diff_bash_tests/base_and_or/and_spec/input.sh

--- srcs/ast/nodes/ast_andor_item/ast_andor_item_del.c
+++ srcs/ast/nodes/ast_andor_item/ast_andor_item_del.c
@@ -16,5 +16,6 @@ void				ast_andor_item_del(t_ast_andor_item *ast_andor_item)
 {
 	twl_lst_del(ast_andor_item->ast_pipe_items, ast_pipe_item_del);
 	token_mgr_del(ast_andor_item->andor_all_tokens);
+	token_del(ast_andor_item->separator);
 	free(ast_andor_item);
 }
