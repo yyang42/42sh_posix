3e677794080190e668354ba087f1cb1c692e844d
yyang42 <yaoyanyang42@gmail.com>
2016-05-28 17:20:31 +0200

    fix/leaks: improve valgrind.sh

--- tools/valgrind.sh
+++ tools/valgrind.sh
@@ -3,10 +3,8 @@
 logfile=valgrind.log
 rm $logfile
 
-for file in `find ./tests/use_case_diff_bash_tests/builtin_getopts -name 'input.sh'`;
+for file in `find ./tests/use_case_diff_bash_tests/$1 -name 'input.sh'`;
 do
-	echo "====================================================================="
-	echo "====================================================================="
 	echo "====================================================================="
 	echo "====================================================================="
 	valgrind --gen-suppressions=all --num-callers=50 ./42sh $file 2>&1 | tee -a $logfile ;
