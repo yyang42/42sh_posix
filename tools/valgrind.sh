622841b614fea302e335b1e4d59ee892eae188d5
yyang42 <yaoyanyang42@gmail.com>
2016-05-28 16:19:51 +0200

    fix/leaks: fix leaks: colon

--- tools/valgrind.sh
+++ tools/valgrind.sh
@@ -3,7 +3,7 @@
 logfile=valgrind.log
 rm $logfile
 
-for file in `find ./tests/use_case_diff_bash_tests/builtin_cd -name 'input.sh'`;
+for file in `find ./tests/use_case_diff_bash_tests/builtin_command -name 'input.sh'`;
 do
 	echo "====================================================================="
 	echo "====================================================================="
