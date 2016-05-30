a31fab17d74e67e59d74f870c0dfc4db7feccace
yyang42 <yaoyanyang42@gmail.com>
2016-05-30 21:18:34 +0200

    fix/bugs: better interactive mode handling 2

--- srcs/shenv/shenv/shenv_is_interactive.c
+++ srcs/shenv/shenv/shenv_is_interactive.c
@@ -14,8 +14,9 @@
 
 bool				shenv_is_interactive(t_shenv *this)
 {
-	if (shenv_shflag_enabled(this, "c")
-		|| (twl_lst_len(this->shenv_argv_remainder) > 0))
+	if (shenv_shflag_enabled(this, "c"))
+		return (false);
+	if (this->shenv_argv_remainder && (twl_lst_len(this->shenv_argv_remainder) > 0))
 		return (false);
 	return (shenv_shflag_enabled(this, "i"));
 }
