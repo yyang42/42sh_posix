36b75951f6eeda4ebb77a7408d1d3ed04a6faa99
yyang42 <yaoyanyang42@gmail.com>
2016-05-31 22:11:51 +0200

    fix/bugs: refactor job control, half done

--- srcs/job_control/job/job_utils_waitpid.c
+++ srcs/job_control/job/job_utils_waitpid.c
@@ -13,6 +13,64 @@
 #include <sys/wait.h>
 #include "shenv/shenv.h"
 #include "shsignal/shsignal.h"
+#include "job_control/job_mgr.h"
+#include "data.h"
+
+static void         handle_notify(pid_t pid)
+{
+	t_job			*job;
+
+	LOG_DEBUG("================= 3 1");
+    job = job_mgr_find_by_pid(shenv_singleton()->jobs, pid);
+    if (job)
+    {
+		LOG_DEBUG("================= 3 2");
+    }
+    if (job && shenv_shflag_enabled(shenv_singleton(), "notify"))
+    {
+    	LOG_DEBUG("================= 4");
+        // if (si_code == CLD_EXITED || si_code == CLD_STOPPED
+        //     || si_code == CLD_KILLED)
+        // {
+            LOG_INFO("si_code=CLD_EXITED, job->id=%d", pid);
+            job_print(job, 0, STDERR_FILENO);
+            job_mgr_remove(shenv_singleton()->jobs, job);
+        // }
+    }
+}
+
+static void			sigstp_catcher(int pid)
+{
+    t_job           *job;
+ //    pid_t           gid;
+
+ //    gid = getpgid(pid);
+ //    LOG_INFO("signum: %d, Signal %d from PID %d, code: %d, value: %d: gid: %d",
+	// signum, info->si_signo, (int)pid, info->si_code, info->si_value, gid);
+ //    LOG_INFO("child stopped: %d", pid);
+    LOG_INFO("data_tmp_jobs() len: %d", twl_lst_len(data_tmp_jobs()));
+    job = job_mgr_find_by_pid(data_tmp_jobs(), pid);
+    if (!job)
+    {
+        LOG_INFO("search in shenv->jobs");
+        job = job_mgr_find_by_pid(shenv_singleton()->jobs, pid);
+    }
+    if (job)
+    {
+        LOG_DEBUG("pid: %d", pid);
+        LOG_DEBUG("job->pid: %d", job->pid);
+        job_mgr_env_push(job);
+        // job_mgr_pop(data_tmp_jobs(), job);
+        // handle_job(sig, job);
+    }
+    else
+    {
+        LOG_INFO("pid not found: %d", pid);
+    }
+    // (void)vp;
+    // (void)signum;
+}
+
 
 void				job_utils_waitpid(pid_t pid, int *res_ptr)
 {
@@ -51,6 +109,7 @@ void				job_utils_waitpid(pid_t pid, int *res_ptr)
 		else if (WIFSTOPPED(res))
 		{
 			LOG_INFO("WIFSTOPPED");
+			sigstp_catcher(pid);
 		}
 		else if (WSTOPSIG(res))
 		{
@@ -60,6 +119,14 @@ void				job_utils_waitpid(pid_t pid, int *res_ptr)
 		{
 			LOG_INFO("UNKNOWN");
 		}
+
+		LOG_DEBUG("================= 1");
+ 		if (WIFSTOPPED(res) || WIFEXITED(res))
+ 		{
+		LOG_DEBUG("================= 2");
+ 			handle_notify(pid);
+ 		}
+
  	}
  	if (res_ptr)
  	{
