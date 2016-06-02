c4679ef40e79acace1e3a7ba46ad6c224e4e67f3
Ludovic <lchenut@student.42.fr>
2016-06-02 09:35:35 +0200

    line edition: remove leaks

--- srcs/data/data_escaped_sequence.c
+++ srcs/data/data_escaped_sequence.c
@@ -15,7 +15,7 @@
 
 t_lst				*data_escaped_sequence(void)
 {
-	t_lst			*data = NULL;
+	static t_lst	*data = NULL;
 
 	if (!data)
 	{
@@ -26,7 +26,6 @@ t_lst				*data_escaped_sequence(void)
 		twl_lst_push_front(data, escaped_sequence_new("\033[D", edit_move_left));
 		twl_lst_push_front(data, escaped_sequence_new("\033[H", edit_move_home));
 		twl_lst_push_front(data, escaped_sequence_new("\033[F", edit_move_end));
-		twl_lst_push_front(data, escaped_sequence_new("\033[3~", edit_del_right));
 		twl_lst_push_front(data, escaped_sequence_new("\033[3~", edit_del_right));
 		twl_lst_push_front(data, escaped_sequence_new("\033[5~", edit_move_up));
 		twl_lst_push_front(data, escaped_sequence_new("\033[6~", edit_move_down));
