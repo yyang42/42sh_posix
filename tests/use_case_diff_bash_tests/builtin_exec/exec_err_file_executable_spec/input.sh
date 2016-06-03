785afdfb7ca070f540103eecb45efa4abbfb3c0c
yyang42 <yaoyanyang42@gmail.com>
2016-06-03 22:15:09 +0200

    fix/bugs_v3: fix builtin_exec/exec_err_file_executable_spec/input.sh

--- tests/use_case_diff_bash_tests/builtin_exec/exec_err_file_executable_spec/input.sh
+++ tests/use_case_diff_bash_tests/builtin_exec/exec_err_file_executable_spec/input.sh
@@ -1,2 +1,3 @@
-touch /tmp/test_builtin_exec_file
-$TESTED_SHELL -c 'exec /tmp/test_builtin_exec_file'
+echo "" > /tmp/test_builtin_exec_executable
+cat -e /tmp/test_builtin_exec_executable
+$TESTED_SHELL -c 'exec /tmp/test_builtin_exec_executable'
