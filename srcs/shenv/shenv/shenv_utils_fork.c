a201c2d13b400cb79cb9be391fba6aa02ece5cd9
yyang42 <yaoyanyang42@gmail.com>
2016-06-04 14:01:33 +0200

    fix/bugs_v4: fix input output error

--- srcs/shenv/shenv/shenv_utils_fork.c
+++ srcs/shenv/shenv/shenv_utils_fork.c
@@ -25,7 +25,7 @@ pid_t					shenv_utils_fork(void)
 	errno_save = errno;
 	if (pid == 0)
 	{
-		twl_lst_clear(shenv_singleton()->jobs, job_del_void);
+		// twl_lst_clear(shenv_singleton()->jobs, job_del_void); // cause read: input/outpur error
 	}
 	else if (pid == -1)
 	{
