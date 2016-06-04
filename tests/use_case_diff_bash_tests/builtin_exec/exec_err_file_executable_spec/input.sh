80f7a9c6fa702990a1bdeb403db11843bc72bb35
yyang42 <yaoyanyang42@gmail.com>
2016-06-04 09:35:32 +0200

    fix/bugs_v3: try fix tests/use_case_diff_bash_tests/builtin_exec/exec_err_file_executable_spec/expected_stdout 2

--- tests/use_case_diff_bash_tests/builtin_exec/exec_err_file_executable_spec/input.sh
+++ tests/use_case_diff_bash_tests/builtin_exec/exec_err_file_executable_spec/input.sh
@@ -1,3 +1,4 @@
 echo "" > /tmp/test_builtin_exec_executable
+chmod 755 /tmp/test_builtin_exec_executable
 cat -e /tmp/test_builtin_exec_executable
 $TESTED_SHELL -c 'exec /tmp/test_builtin_exec_executable'
