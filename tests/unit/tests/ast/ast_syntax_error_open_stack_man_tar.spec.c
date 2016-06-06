75bd726f332a2a20298db43d935efe97975015c5
yyang42 <yaoyanyang42@gmail.com>
2016-06-06 21:37:33 +0200

    fix/bugs_v4: handle

---
+++ tests/unit/tests/ast/ast_syntax_error_open_stack_man_tar.spec.c
@@ -0,0 +1,10 @@
+#include <project.h>
+
+#include "ast/ast.h"
+
+mt_test_ast_open_stack(01, "BSDTAR(1)                 BSD General Commands Manual                BSDTAR(1)", "", true);
+
+void	suite_ast_syntax_error_open_stack_man_tar(t_suite *suite)
+{
+	SUITE_ADD_TEST(suite, test_01);
+}
