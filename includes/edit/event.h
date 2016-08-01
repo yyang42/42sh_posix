45c6bef58c052626fd66d16d26afe9045eb59fdf
Ludovic CHENUT <chenut.ludovic@gmail.com>
2016-08-01 13:05:43 +0200

    line edition: begin the event designator part

---
+++ includes/edit/event.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_norris_loves_the_norminette.c                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chuck <chuck@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2042/02/30 42:00:00 by chuck             #+#    #+#             */
+/*   Updated: 2042/02/30 41:59:59 by chuck            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EVENT_H
+# define EVENT_H
+
+#include "utils.h"
+#include "edit/edit.h"
+
+typedef struct			s_event
+{
+	t_edit				*edit;
+}						t_event;
+
+t_event					*event_new(t_edit *edit);
+void					event_del(t_event *this);
+
+char					*event_expand(t_edit *edit);
+
+#endif
