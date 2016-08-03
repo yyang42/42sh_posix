db6d3c1e668628ebc36677c6aa23ae47950160aa
Ludovic CHENUT <chenut.ludovic@gmail.com>
2016-08-03 11:35:11 +0200

    line edition: event designator finished !?string[?] this pattern

--- srcs/edit/edit/event/event/event_expand_command_contain.c
+++ srcs/edit/edit/event/event/event_expand_command_contain.c
@@ -12,9 +12,50 @@
 
 #include "edit/event.h"
 
+static char		*get_string_fn(char *token)
+{
+	char		*ret;
+	size_t		index;
+
+	ret = twl_strnew(twl_strlen(token));
+	index = 0;
+	while (*token && *token != '?')
+	{
+		ret[index] = *token;
+		index += 1;
+		token += 1;
+	}
+	ret[index] = 0;
+	return (ret);
+}
+
+static bool		find_fn(void *data, void *ctx)
+{
+	char		*haystack;
+	char		*needle;
+
+	haystack = ((t_line *)data)->copy;
+	needle = ctx;
+	return (twl_strstr(haystack, needle));
+}
+
 void			event_expand_command_contain(t_event *this,
 					t_event_token *token)
 {
-	(void)this;
-	(void)token;
+	t_line		*line;
+	char		*tok;
+
+	this->expand = true;
+	tok = get_string_fn(token->token + 2);
+	if (*tok == 0)
+		event_print_error(this, token);
+	else
+	{
+		line = twl_lst_find(this->edit->history, find_fn, tok);
+		if (!line)
+			event_print_error(this, token);
+		else
+			event_concat_string(this, line->copy);
+	}
+	free(tok);
 }
