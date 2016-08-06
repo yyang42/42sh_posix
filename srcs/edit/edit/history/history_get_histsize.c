1a82766ef33ba2b4359a5fadb342b0d469096621
Ludovic CHENUT <chenut.ludovic@gmail.com>
2016-08-06 12:58:32 +0200

    history: added one method use for get the history size

---
+++ srcs/edit/edit/history/history_get_histsize.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_norris_loves_the_norminette.c                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chuck <chuck@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2042/02/30 42:00:00 by chuck             #+#    #+#             */
+/*   Updated: 2042/02/30 41:59:59 by chuck            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "edit/history.h"
+#include "twl_stdlib.h"
+
+static bool			is_shvar_number(char *shvar)
+{
+	while (*shvar == ' ' || *shvar == '\t' || *shvar == '\n')
+		shvar += 1;
+	if (!*shvar)
+		return (false);
+	while (*shvar >= '0' && *shvar <= '9')
+		shvar += 1;
+	while (*shvar == ' ' || *shvar == '\t' || *shvar == '\n')
+		shvar += 1;
+	return (*shvar == '\0');
+}
+
+void				history_get_histsize(t_history *this)
+{
+	char			*shvar;
+
+	shvar = shenv_shvars_get_value(shenv_singleton(), "HISTSIZE");
+	if (!shvar)
+		this->total = DFL_HISTSIZE;
+	else if (!*shvar || is_shvar_number(shvar))
+		return ;
+	else
+		this->total = (size_t)twl_atoi(shvar);
+	while (this->length > this->total)
+		history_pop(this);
+}
