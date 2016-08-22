38d5394b34168f9ded47fbf9369cce2c83244432
Ludovic CHENUT <chenut.ludovic@gmail.com>
2016-08-22 16:29:22 +0200

    fix/completion: add the completion executable with home directory

--- srcs/edit/completion/completion_exec.c
+++ srcs/edit/completion/completion_exec.c
@@ -12,9 +12,30 @@
 
 #include "edit/completion.h"
 
+static void		completion_exec_from_home(t_completion *this)
+{
+	char		*tmp;
+
+	if (this->current_word[1] == 0)
+		edit_place_letter(this->edit, '/');
+	else if (this->current_word[1] == '/')
+	{
+		tmp = this->current_word;
+		this->current_word = twl_strjoin(shenv_get_home(shenv_singleton()),
+				this->current_word + 1);
+		if (this->current_word[0] != '~')
+			completion_exec(this);
+		free(this->current_word);
+		this->current_word = tmp;
+	}
+	twl_lst_clear(this->all, free);
+}
+
 void			completion_exec(t_completion *this)
 {
-	if (completion_utils_exec_absolute_path(this))
+	if (this->current_word[0] == '~')
+		completion_exec_from_home(this);
+	else if (completion_utils_exec_absolute_path(this))
 	{
 		if (this->current_word[0] == '/')
 			completion_exec_from_root(this);
