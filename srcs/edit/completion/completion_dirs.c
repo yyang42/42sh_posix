b69023107b8d59e6c726b47a1b3b02199897a042
Ludovic CHENUT <chenut.ludovic@gmail.com>
2016-08-22 16:10:54 +0200

    fix/completion: add the completion argument with home directory

--- srcs/edit/completion/completion_dirs.c
+++ srcs/edit/completion/completion_dirs.c
@@ -12,10 +12,31 @@
 
 #include "edit/completion.h"
 
+static void		completion_dirs_from_home(t_completion *this)
+{
+	char		*tmp;
+
+	if (this->current_word[1] == 0)
+		edit_place_letter(this->edit, '/');
+	else if (this->current_word[1] == '/')
+	{
+		tmp = this->current_word;
+		this->current_word = twl_strjoin(shenv_get_home(shenv_singleton()),
+				this->current_word + 1);
+		if (this->current_word[0] != '~')
+			completion_dirs(this);
+		free(this->current_word);
+		this->current_word = tmp;
+	}
+	twl_lst_clear(this->all, free);
+}
+
 void			completion_dirs(t_completion *this)
 {
 	if (this->current_word[0] == '/')
 		completion_dirs_from_root(this);
+	else if (this->current_word[0] == '~')
+		completion_dirs_from_home(this);
 	else
 		completion_dirs_from_cwd(this);
 }
