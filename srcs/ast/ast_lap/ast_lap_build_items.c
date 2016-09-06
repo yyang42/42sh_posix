187c8c3f570c61d686ffa77b05c91d89f1a43f71
Ludovic CHENUT <chenut.ludovic@gmail.com>
2016-09-06 11:09:50 +0200

    norme srcs/ast/ast_lap/ast_lap_build_items.c

--- srcs/ast/ast_lap/ast_lap_build_items.c
+++ srcs/ast/ast_lap/ast_lap_build_items.c
@@ -28,10 +28,6 @@ static bool	is_type_separator(t_ast_type type, t_token *token)
 			twl_strequ_void, token->text));
 }
 
-/*
-** Special case for list items
-*/
-
 static bool	should_break_fn(t_lst *tokens, t_ast_type type, t_token *last_sep)
 {
 	if (!twl_lst_first(tokens))
@@ -45,13 +41,29 @@ static bool	should_break_fn(t_lst *tokens, t_ast_type type, t_token *last_sep)
 	return (false);
 }
 
+static t_lst	*ast_lap_build_items_end_fn(t_lst *tokens, t_ast_type type,
+				struct s_ast *ast, t_ast_lap *l)
+{
+	if (token_mgr_first_equ(tokens, "("))
+	{
+		ast_set_error_msg_syntax_error_near(ast, token_mgr_first(tokens), NULL);
+		twl_lst_del(l->container, ast_lap_del_fns()[type]);
+		return (NULL);
+	}
+	if (is_last_sep_that_require_more_tokens(l->last_sep))
+	{
+		ast_add_to_open_stack(ast, l->last_sep->text);
+		ast_set_error_msg_syntax_error_near(ast, l->last_sep, NULL);
+		twl_lst_del(l->container, ast_lap_del_fns()[type]);
+		return (NULL);
+	}
+	return (l->container);
+}
+
 t_lst		*ast_lap_build_items(t_lst *tokens,
 							t_ast_type type, struct s_ast *ast)
 {
 	t_ast_lap		l;
-	//void			*item;
-	//t_lst			*container;
-	//t_token			*last_sep;
 
 	l.last_sep = NULL;
 	l.container = twl_lst_new();
@@ -75,18 +87,5 @@ t_lst		*ast_lap_build_items(t_lst *tokens,
 		ast_lap_set_separator_fns()[type](l.item, token_mgr_first(tokens));
 		l.last_sep = twl_lst_pop_front(tokens);
 	}
-	if (token_mgr_first_equ(tokens, "("))
-	{
-		ast_set_error_msg_syntax_error_near(ast, token_mgr_first(tokens), NULL);
-		twl_lst_del(l.container, ast_lap_del_fns()[type]);
-		return (NULL);
-	}
-	if (is_last_sep_that_require_more_tokens(l.last_sep))
-	{
-		ast_add_to_open_stack(ast, l.last_sep->text);
-		ast_set_error_msg_syntax_error_near(ast, l.last_sep, NULL);
-		twl_lst_del(l.container, ast_lap_del_fns()[type]);
-		return (NULL);
-	}
-	return (l.container);
+	return (ast_lap_build_items_end_fn(tokens, type, ast, &l));
 }
