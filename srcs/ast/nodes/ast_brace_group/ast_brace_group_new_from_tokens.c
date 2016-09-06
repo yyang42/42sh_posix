5d5738c940dd48db83be940f2d7eaf997d27d6fa
Ludovic CHENUT <chenut.ludovic@gmail.com>
2016-09-06 12:03:11 +0200

    norme: srcs/ast/nodes/ast_brace_group

--- srcs/ast/nodes/ast_brace_group/ast_brace_group_new_from_tokens.c
+++ srcs/ast/nodes/ast_brace_group/ast_brace_group_new_from_tokens.c
@@ -14,40 +14,57 @@
 #include "ast/nodes/ast_brace_group.h"
 #include "ast/nodes/ast_compound_list.h"
 
-t_ast_brace_group	*ast_brace_group_new_from_tokens(t_lst *tokens,
-	struct s_ast *ast)
+static inline t_ast_brace_group	*ast_brace_group_new_from_tokens_init(
+		t_lst *tokens, struct s_ast *ast, t_token **open)
 {
-	t_ast_brace_group		*ast_brace_group;
-	t_token					*open;
+	t_ast_brace_group	*abg;
 
-	ast_brace_group = ast_brace_group_new();
-	open = twl_lst_pop_front(tokens);
-	ast_brace_group->ast_compound_list = ast_compound_list_new_from_tokens(tokens, ast);
+	abg = ast_brace_group_new();
+	*open = twl_lst_pop_front(tokens);
+	abg->ast_compound_list =
+		ast_compound_list_new_from_tokens(tokens, ast);
 	if (ast_has_error(ast))
 	{
-		ast_brace_group_del(ast_brace_group);
+		ast_brace_group_del(abg);
 		return (NULL);
 	}
+	return (abg);
+}
+
+static inline t_ast_brace_group	*ast_brace_group_new_from_tokens_end(
+		t_lst *tokens, t_ast_brace_groupe *abg)
+{
+	if (abg->ast_compound_list == NULL)
+	{
+		ast_brace_group_del(abg);
+		return (NULL);
+	}
+	twl_lst_pop_front(tokens);
+	return (abg);
+}
+
+t_ast_brace_group				*ast_brace_group_new_from_tokens(
+		t_lst *tokens, struct s_ast *ast)
+{
+	t_ast_brace_group	*abg;
+	t_token				*open;
+
+	if (!(abg = ast_brace_group_new_from_tokens_init(tokens, ast, &open)))
+		return (NULL);
 	if (token_mgr_first_equ(tokens, "}") == false)
 	{
 		ast_add_to_open_stack(ast, "{");
 		ast_set_error_msg_syntax_error_near(ast, open, NULL);
-		ast_brace_group_del(ast_brace_group);
-		return NULL;
+		ast_brace_group_del(abg);
+		return (NULL);
 	}
-	if (ast_brace_group->ast_compound_list
-		&& twl_lst_len(ast_brace_group->ast_compound_list->ast_list_items) == 0)
+	if (abg->ast_compound_list
+		&& twl_lst_len(abg->ast_compound_list->ast_list_items) == 0)
 	{
 		ast_set_error_msg_syntax_error_near(ast, open,
 			"Brace group missing compound list");
-		ast_brace_group_del(ast_brace_group);
+		ast_brace_group_del(abg);
 		return (NULL);
 	}
-	if (ast_brace_group->ast_compound_list == NULL)
-	{
-		ast_brace_group_del(ast_brace_group);
-		return (NULL);
-	}
-	twl_lst_pop_front(tokens);
-	return (ast_brace_group);
+	return (ast_brace_group_new_from_tokens_end(tokens, abg));
 }
