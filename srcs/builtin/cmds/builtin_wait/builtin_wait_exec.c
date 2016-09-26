9a903521bf64960e787dcba6e77151472d3f9108
Ludovic CHENUT <chenut.ludovic@gmail.com>
2016-09-26 14:44:04 +0200

    norme: builtin/cmds/builtin_wait

--- srcs/builtin/cmds/builtin_wait/builtin_wait_exec.c
+++ srcs/builtin/cmds/builtin_wait/builtin_wait_exec.c
@@ -13,39 +13,42 @@
 #include "builtin/cmds/builtin_wait.h"
 #include <setjmp.h>
 
-static jmp_buf jump_buf;
+static jmp_buf		g_jump_buf;
 
-static void         sigint_handler(int sig)
+static void			sigint_handler(int sig)
 {
-    LOG_INFO("SIGINT handler: %d", sig);
-    longjmp(jump_buf, 1);
-    (void)sig;
+	LOG_INFO("SIGINT handler: %d", sig);
+	longjmp(g_jump_buf, 1);
+	(void)sig;
 }
 
-void                builtin_wait_exec(t_lst *tokens, t_shenv *shenv)
-{
-    t_argparser_result *argparser_result;
+/*
+** TODO: Avoid jump if possible
+*/
 
-    argparser_result = argparser_parse_tokens(builtin_wait_argparser(), tokens);
-    shenv->shenv_cur_token = token_mgr_first(tokens);
-    if (argparser_result->err_msg)
-    {
-        argparser_result_print_error_with_help(argparser_result);
-        shenv->last_exit_code = EXIT_FAILURE;
-    }
-    else
-    {
-        signal(SIGINT, sigint_handler);
-        // TODO: Avoid jump if possible
-        if (setjmp(jump_buf) == 0)
-        {
-            builtin_wait_exec_segs(argparser_result);
-        }
-        else
-        {
-            shenv->last_exit_code = EXIT_FAILURE;
-        }
-        signal(SIGINT, SIG_IGN);
-    }
-    argparser_result_del(argparser_result);
+void				builtin_wait_exec(t_lst *tokens, t_shenv *shenv)
+{
+	t_argparser_result *argparser_result;
+
+	argparser_result = argparser_parse_tokens(builtin_wait_argparser(), tokens);
+	shenv->shenv_cur_token = token_mgr_first(tokens);
+	if (argparser_result->err_msg)
+	{
+		argparser_result_print_error_with_help(argparser_result);
+		shenv->last_exit_code = EXIT_FAILURE;
+	}
+	else
+	{
+		signal(SIGINT, sigint_handler);
+		if (setjmp(g_jump_buf) == 0)
+		{
+			builtin_wait_exec_segs(argparser_result);
+		}
+		else
+		{
+			shenv->last_exit_code = EXIT_FAILURE;
+		}
+		signal(SIGINT, SIG_IGN);
+	}
+	argparser_result_del(argparser_result);
 }
