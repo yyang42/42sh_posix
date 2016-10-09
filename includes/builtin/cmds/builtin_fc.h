34212aa91e71b2acb8536610fc67556e4511e3bf
Ludovic CHENUT <chenut.ludovic@gmail.com>
2016-10-09 13:27:51 +0200

    feature/fc: wrapper done

---
+++ includes/builtin/cmds/builtin_fc.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_norris_loves_the_norminette.c                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chuck <chuck@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2042/02/30 42:00:00 by chuck             #+#    #+#             */
+/*   Updated: 2042/02/30 41:59:59 by chuck            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUILTIN_FC_H
+# define BUILTIN_FC_H
+
+# include "basics.h"
+# include "builtin/builtin.h"
+
+void				builtin_fc_exec(t_lst *tokens, t_shenv *env);
+
+#endif
