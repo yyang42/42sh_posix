abee0489f30f6e906e9bd6901303dc732ebbebf2
Ludovic CHENUT <chenut.ludovic@gmail.com>
2016-10-12 13:24:49 +0200

    fix/remove_forbidden_functions: remove the strsignal call

--- srcs/signals/handle_exec_signal.c
+++ srcs/signals/handle_exec_signal.c
@@ -19,6 +19,53 @@
 
 #define SIG_EXIT_CODE_MIN 128
 
+static const char	*g_strsignal_list[] =
+{
+	"Unknown signal",
+	"Hangup",
+	"Interrupt",
+	"Quit",
+	"Illegal instruction",
+	"Trace/BPT trap",
+	"Abort trap",
+	"EMT trap",
+	"Floating point exception",
+	"Killed",
+	"Bus error",
+	"Segmentation fault",
+	"Bad system call",
+	"Broken pipe",
+	"Alarm clock",
+	"Terminated",
+	"Urgent I/O condition",
+	"Suspended (signal)",
+	"Suspended",
+	"Continued",
+	"Child exited",
+	"Stopped (tty input)",
+	"Stopped (tty output)",
+	"I/O possible",
+	"Cputime limit exceeded",
+	"Filesize limit exceeded",
+	"Virtual timer expired",
+	"Profiling timer expired",
+	"Window size changes",
+	"Information request",
+	"User defined signal 1",
+	"User defined signal 2",
+	NULL
+};
+static char			g_strsignal_ret[1024];
+
+static char			*get_signal(int sig)
+{
+	if (sig < 0 || sig > 32 || g_strsignal_list[sig] == NULL)
+		twl_sprintf(g_strsignal_ret, "Unknown signal: %i%c", sig, 0);
+	else
+		twl_sprintf(g_strsignal_ret, "%s: %i%c", g_strsignal_list[sig], sig, 0);
+	return (g_strsignal_ret);
+}
+
 static int			get_exit_code(int sig)
 {
 	return (SIG_EXIT_CODE_MIN + sig);
@@ -28,7 +75,7 @@ static void			print_error_msg(int sig)
 {
 	char		*msg;
 
-	msg = strsignal(sig);
+	msg = get_signal(sig);
 	if (!msg)
 	{
 		LOG_ERROR("strsignal: %s", twl_strerror(errno));
