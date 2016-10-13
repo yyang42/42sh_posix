63e037a6e5fecb243aac3f641ea27425d5b437fc
Ludovic CHENUT <chenut.ludovic@gmail.com>
2016-10-13 18:29:14 +0200

    feature/builtin_fc: -s option nearly done

--- srcs/builtin/cmds/builtin_fc/builtin_fc_exec.c
+++ srcs/builtin/cmds/builtin_fc/builtin_fc_exec.c
@@ -14,41 +14,7 @@
 #include "edit/history.h"
 #include "edit/edit.h"
 #include "twl_stdlib.h"
-
-static char	*get_first_occurence(char *str, char *old, size_t *size_old)
-{
-	size_t	index;
-
-	index = 0;
-	while (str[index])
-	{
-		*size_old = 0;
-		while (str[index + *size_old] == old[*size_old] && old[*size_old])
-			*size_old += 1;
-		if (!old[*size_old])
-			break ;
-		index += 1;
-	}
-	if (!str[index])
-		return (NULL);
-	return (str + index);
-}
-
-char		*twl_strreplace_first(char *str, char *old, char *new)
-{
-	size_t	size_old;
-	char	*first_occurence;
-	char	*ret;
-
-	first_occurence = get_first_occurence(str, old, &size_old);
-	if (!first_occurence)
-		return (twl_strdup(str));
-	ret = twl_strnew(twl_strlen(str) - size_old + twl_strlen(new));
-	twl_strncpy(ret, str, first_occurence - str);
-	twl_strcat(ret, new);
-	twl_strcat(ret, first_occurence + size_old);
-	return (ret);
-}
+#include "ast/ast.h"
 
 char		*builtin_fc_replace_pattern(char *command, char *pattern)
 {
@@ -64,7 +30,7 @@ char		*builtin_fc_replace_pattern(char *command, char *pattern)
 		index += 1;
 	old = twl_strndup(pattern, index);
 	new = pattern[index] ? pattern + index + 1 : pattern + index;
-	ret = twl_strreplace_first(command, old, new);
+	ret = twl_str_replace_first(command, old, new);
 	free(old);
 	return (ret);
 }
@@ -82,7 +48,6 @@ static char		*get_line_to_execute(t_argparser_result *result)
 {
 	char		*pattern;
 	char		*command_index;
-	char		*command;
 
 	pattern = twl_lst_first(result->remainders);
 	if (!pattern || !twl_strchr(pattern, '='))
@@ -98,17 +63,21 @@ static char		*get_line_to_execute(t_argparser_result *result)
 		return (get_command_from_index(-1, pattern));
 	else if (twl_str_is_num(command_index))
 		return (get_command_from_index(twl_atoi(command_index), pattern));
+//	else
+//		return (get_command_from_command_start(command_index, pattern));
 	return (NULL);
-	(void)result;
-(void)pattern;
-(void)command_index;//	else
-(void)command;      //		return (get_command_from_command_start(command_index, pattern));
 }
 
 void			builtin_fc_reexecute(t_argparser_result *result)
 {
+	char		*command;
+
 	history_pop_last(edit_singleton()->history);
-	twl_printf("%s\n", get_line_to_execute(result));
+	command = get_line_to_execute(result);
+	twl_printf("<%s>\n", command);
+	history_push(edit_singleton()->history, line_new_from_string(command));
+	ast_utils_exec_string(command, 1);
+	free(command);
 	(void)result;
 }
 
