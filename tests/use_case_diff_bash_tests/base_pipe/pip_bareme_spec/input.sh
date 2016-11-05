b51a31526c12304dc6b6891b3c4fd9daaeda4eef
yyang42 <yang.contact@gmail.com>
2016-11-05 17:02:50 +0100

    fix_pipe_final: done

--- tests/use_case_diff_bash_tests/base_pipe/pip_bareme_spec/input.sh
+++ tests/use_case_diff_bash_tests/base_pipe/pip_bareme_spec/input.sh
@@ -1,6 +1,11 @@
+echo "===== 1"
 python -c 'print "A"*100000' > /tmp/pip_bareme_spec_file
+echo "===== 2"
 cat /tmp/pip_bareme_spec_file | head -c 2 | wc
+echo "===== 3"
 python -c 'print "A"*200000' > /tmp/pip_bareme_spec_file
+echo "===== 4"
 cat /tmp/pip_bareme_spec_file | head -c 2 | wc
+echo "===== 5"
 # python -c 'print "A"*81920' | head -c 2 | wc
 # python -c 'print "abcde"' | /bin/echo
