0ee3bdc212c6b79779e6f58e34f7a98d06b914bd
yyang42 <yang.contact@gmail.com>
2016-11-06 17:05:33 +0100

    fix_job_control_segv: done

--- srcs/job_control/jobexec/jobexec_fork_utils_init_sigchld_handler.c
+++ srcs/job_control/jobexec/jobexec_fork_utils_init_sigchld_handler.c
@@ -89,6 +89,7 @@ static void			sigstp_catcher(int signum, siginfo_t *info, void *vp)
 		job_mgr_env_push(job);
 		shenv_singleton()->shenv_sigtstp_trigerred = true;
 	}
+	shenv_singleton()->shenv_foreground_job = NULL;
 	(void)vp;
 	(void)signum;
 	(void)handle_job;
