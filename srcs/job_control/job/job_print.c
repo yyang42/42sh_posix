eb71cbe450ebfa9798e6828aaab85a2e5ac911ab
Yaoyan Yan YANG <yaoyan.yang@blablacar.com>
2017-10-15 17:06:11 +0200

    fix logical not parent

--- srcs/job_control/job/job_print.c
+++ srcs/job_control/job/job_print.c
@@ -30,7 +30,7 @@ void				job_print(t_job *this, int flags, int fd)
 	else
 	{
 		twl_dprintf(fd, "[%lld]%c ", this->job_id, this->sign);
-		if (!flags & BUILTIN_JOBS_FLAG_OPT_L)
+		if (!(flags & BUILTIN_JOBS_FLAG_OPT_L))
 			twl_dprintf(fd, " ");
 		full_status = job_status_str_long(this,
 				flags & BUILTIN_JOBS_FLAG_OPT_L);
